d5e70b731f9e3695f3d94116ace9f6f3474007d9
neilpeterson <neilpeterson@outlook.com>
2017-02-09 10:56:52 -0800

    scritp updates

--- virtual-machine/create-vm-monitor-oms/create-vm-monitor-oms.sh
+++ virtual-machine/create-vm-monitor-oms/create-vm-monitor-oms.sh
@@ -1,63 +1,49 @@
 #!/bin/sh
 
-# Variables
-resourceGroupName=myResourceGroup
-location=westeurope
-storageaccount=mystorageaccount$RANDOM
-publicdns=mypublicdns$RANDOM
-
-# Update with OMS Id and OMS key.
-omsid=myOMSId
-omskey=myOMSKey
+# OMS Id and OMS key.
+omsid=<Replace with your OMS Id>
+omskey=<Replace with your OMS key>
 
 # Create a resource group.
-az group create --name $resourceGroupName --location $location
-
-# Create a storage account.
-az storage account create --resource-group $resourceGroupName --location $location \
-  --name $storageaccount --kind Storage --sku Standard_LRS
+az group create --name myResourceGroup --location westeurope
 
 # Create a virtual network.
-az network vnet create --resource-group $resourceGroupName --location $location --name myVnet \
+az network vnet create --resource-group myResourceGroup --location westeurope --name myVnet \
   --address-prefix 192.168.0.0/16 --subnet-name mySubnet --subnet-prefix 192.168.1.0/24
 
 # Create a public IP address and specify a DNS name.
-az network public-ip create --resource-group $resourceGroupName --location $location \
-  --name myPublicIP --dns-name $publicdns --allocation-method static --idle-timeout 4
+az network public-ip create --resource-group myResourceGroup --location westeurope \
+  --name myPublicIP --dns-name publicdns=mypublicdns$RANDOM --allocation-method static --idle-timeout 4
 
 # Create a network security group.
-az network nsg create --resource-group $resourceGroupName --location $location \
+az network nsg create --resource-group myResourceGroup --location westeurope \
   --name myNetworkSecurityGroup
 
 # Create an inbound network security group rule for port 22.
-az network nsg rule create --resource-group $resourceGroupName \
+az network nsg rule create --resource-group myResourceGroup \
   --nsg-name myNetworkSecurityGroup --name myNetworkSecurityGroupRuleSSH \
   --protocol tcp --direction inbound --priority 1000 --source-address-prefix '*' \
   --source-port-range '*' --destination-address-prefix '*' --destination-port-range 22 \
   --access allow
 
 # Create a virtual network card and associate with public IP address and NSG.
-az network nic create --resource-group $resourceGroupName --location $location --name myNic1 \
-  --vnet-name myVnet --subnet mySubnet --network-security-group myNetworkSecurityGroup \
+az network nic create --resource-group myResourceGroup --location westeurope --name myNic \
+  --vnet-name myNic1 --subnet mySubnet --network-security-group myNetworkSecurityGroup \
   --public-ip-address myPublicIP
 
 # Create a virtual machine. 
 az vm create \
-  --resource-group $resourceGroupName \
+  --resource-group myResourceGroup \
   --name myVM1 \
-  --location $location \
+  --location westeurope \
   --nics myNic1 \
-  --vnet myVnet \
-  --subnet-name mySubnet \
-  --nsg myNetworkSecurityGroup \
-  --storage-account $storageaccount \
   --image UbuntuLTS \
   --ssh-key-value ~/.ssh/id_rsa.pub \
-  --admin-username ops
+  --admin-username azureuser
 
 # Install and configure the OMS agent.
 az vm extension set \
-  --resource-group $resourceGroupName \
+  --resource-group myResourceGroup \
   --vm-name myVM1 --name OmsAgentForLinux \
   --publisher Microsoft.EnterpriseCloud.Monitoring \
   --version 1.0 --protected-settings '{"workspaceKey": "$omskey"}' \
--- virtual-machine/create-vm-nsg/create-vm-nsg.sh
+++ virtual-machine/create-vm-nsg/create-vm-nsg.sh
@@ -1,106 +1,90 @@
 #!/bin/bash
 
-# Variables
-resourceGroupName=myResourceGroup
-location=westeurope
-storageaccount=mystorageaccount$RANDOM
-publicdns=mypublicdns$RANDOM
-
 # Create a resource group.
-az group create --name $resourceGroupName --location $location
-
-# Create a storage account.
-az storage account create --resource-group $resourceGroupName --location $location \
-  --name $storageaccount --kind Storage --sku Standard_LRS
+az group create --name myResourceGroup --location westeurope
 
 # Create a virtual network and subnet (front end).
-az network vnet create --resource-group $resourceGroupName --location $location --name myVnet \
+az network vnet create --resource-group myResourceGroup --location westeurope --name myVnet \
   --address-prefix 192.168.0.0/16 --subnet-name mySubnetFrontEnd --subnet-prefix 192.168.1.0/24
 
 # Create a subnet (back end) and associate with virtual network. 
-az network vnet subnet create --resource-group $resourceGroupName --vnet-name myVnet \
+az network vnet subnet create --resource-group myResourceGroup --vnet-name myVnet \
   --name mySubnetBackEnd --address-prefix 192.168.2.0/24
 
 # Create a public IP address and specify a DNS name.
-az network public-ip create --resource-group $resourceGroupName --location $location \
-  --name myPublicIP --dns-name $publicdns --allocation-method static --idle-timeout 4
+az network public-ip create --resource-group myResourceGroup --location westeurope \
+  --name myPublicIP --dns-name mypublicdns$RANDOM --allocation-method static --idle-timeout 4
 
 # Create a network security group.
-az network nsg create --resource-group $resourceGroupName --location $location \
+az network nsg create --resource-group myResourceGroup --location westeurope \
   --name myNetworkSecurityGroupFrontEnd
 
 # Create an inbound network security group rule for port 22.
-az network nsg rule create --resource-group $resourceGroupName \
+az network nsg rule create --resource-group myResourceGroup \
   --nsg-name myNetworkSecurityGroupFrontEnd --name myNetworkSecurityGroupRuleFrontEndSSH \
   --protocol tcp --direction inbound --priority 1000 --source-address-prefix '*' \
   --source-port-range '*' --destination-address-prefix '*' --destination-port-range 22 \
   --access allow
 
 # Create an inbound network security group rule for port 80.
-az network nsg rule create --resource-group $resourceGroupName \
+az network nsg rule create --resource-group myResourceGroup \
   --nsg-name myNetworkSecurityGroupFrontEnd --name myNetworkSecurityGroupRuleFrontEndHTTP \
   --protocol tcp --direction inbound --priority 1001 --source-address-prefix '*' \
   --source-port-range '*' --destination-address-prefix '*' --destination-port-range 80 \
   --access allow
 
 # Associate network security group with subnet. 
-az network vnet subnet update --resource-group $resourceGroupName --vnet-name myVnet \
+az network vnet subnet update --resource-group myResourceGroup --vnet-name myVnet \
   --name mySubnetFrontEnd --network-security-group myNetworkSecurityGroupFrontEnd
 
 # Create a network security group.
-az network nsg create --resource-group $resourceGroupName --location $location \
+az network nsg create --resource-group myResourceGroup --location westeurope \
   --name myNetworkSecurityGroupBackEnd
 
 # Create an inbound network security group rule for port 22.
-az network nsg rule create --resource-group $resourceGroupName \
+az network nsg rule create --resource-group myResourceGroup \
   --nsg-name myNetworkSecurityGroupBackEnd --name myNetworkSecurityGroupRuleBackEndSSH \
   --protocol tcp --direction inbound --priority 1000 --source-address-prefix 192.168.1.0/24 \
   --source-port-range '*' --destination-address-prefix '*' --destination-port-range 22 \
   --access allow
 
 # Create an inbound network security group rule for port 27017.
-az network nsg rule create --resource-group $resourceGroupName \
+az network nsg rule create --resource-group myResourceGroup \
   --nsg-name myNetworkSecurityGroupBackEnd --name myNetworkSecurityGroupRuleBackEndMongoDB \
   --protocol tcp --direction inbound --priority 1001 --source-address-prefix 192.168.1.0/24 \
   --source-port-range '*' --destination-address-prefix '*' --destination-port-range 27017 \
   --access allow
 
 # Associate network security group with subnet. 
-az network vnet subnet update --resource-group $resourceGroupName --vnet-name myVnet \
+az network vnet subnet update --resource-group myResourceGroup --vnet-name myVnet \
   --name mySubnetBackEnd --network-security-group myNetworkSecurityGroupBackEnd
 
 # Create a virtual network card and associate with front end subnet and public IP address.
-az network nic create --resource-group $resourceGroupName --location $location --name myNic1 \
+az network nic create --resource-group myResourceGroup --location westeurope --name myNic1 \
     --vnet-name myVnet --subnet mySubnetFrontEnd --public-ip-address myPublicIP
 
 # Create a virtual network card and associate with back end subnet.
-az network nic create --resource-group $resourceGroupName --location $location --name myNic2 \
+az network nic create --resource-group myResourceGroup --location westeurope --name myNic2 \
     --vnet-name myVnet --subnet mySubnetBackEnd
 
 # Create a virtual machine. 
 az vm create \
-    --resource-group $resourceGroupName \
+    --resource-group myResourceGroup \
     --name myVMFrontEnd \
-    --location $location \
+    --location westeurope \
     --nics myNic1 \
-    --vnet myVnet \
-    --subnet-name mySubnetFrontEnd \
-    --storage-account $storageaccount \
     --image UbuntuLTS \
     --ssh-key-value ~/.ssh/id_rsa.pub \
-    --admin-username ops \
+    --admin-username azureuser \
     --no-wait
 
 # Create a virtual machine. 
 az vm create \
-    --resource-group $resourceGroupName \
+    --resource-group myResourceGroup \
     --name myVMBackEnd \
-    --location $location \
+    --location westeurope \
     --nics myNic2\
-    --vnet myVnet \
-    --subnet-name mySubnetBackEnd \
-    --storage-account $storageaccount \
     --image UbuntuLTS \
     --ssh-key-value ~/.ssh/id_rsa.pub \
-    --admin-username ops \
+    --admin-username azureuser \
     --no-wait
