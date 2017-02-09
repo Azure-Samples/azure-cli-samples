31e68ba4346b0284acab00b9c87df9dc0c0846a1
Cory Fowler <cfowler@microsoft.com>
2017-02-09 11:54:35 -0800

    updated scripts with comments

--- app-service/scale-manual/setup.sh
+++ app-service/scale-manual/setup.sh
@@ -1,12 +1,21 @@
 #/bin/bash
 
+# Ensures unique id
 random=$(python -c 'import uuid; print(str(uuid.uuid4())[0:8])')
+
+# Variables
 resourceGroupName="myResourceGroup$random"
 appName="AppServiceManualScale$random"
 location="WestUS"
 
-az group create --name $resourceGroupName --location $location
+# Create a Resource Group
+az group create --name $resourceGroupName --location $location1
+
+# Create App Service Plans
 az appservice plan create --name AppServiceManualScalePlan --resource-group $resourceGroupName --location $location --sku B1
+
+# Add a Web App
 az appservice web create --name $appName --plan AppServiceManualScalePlan --resource-group $resourceGroupName
 
+# Scale Web App to 2 Workers
 az appservice plan update --number-of-workers 2 --name AppServiceManualScalePlan --resource-group $resourceGroupName
