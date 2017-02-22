f3c1285004385e841dc049bf05a5b4500e2ac6e9
Cephas Lin <cephalin@microsoft.com>
2017-02-22 16:09:45 +0100

    enable private GitHub repo

--- app-service/deploy-github/deploy-github.sh
+++ app-service/deploy-github/deploy-github.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 gitrepo=<Replace with your GitHub repo URL>
+token=<Replace with a GitHub access token>
 webappname=mywebapp$RANDOM
 
 # Create a resource group.
@@ -12,9 +13,10 @@ az appservice plan create --name $webappname --resource-group $webappname --sku 
 # Create a web app.
 az appservice web create --name $webappname --resource-group $webappname --plan $webappname
 
-# Configure GitHub deployment from your GitHub repo and deploy once.
+# Configure GitHub deployment. 
+# --git-token parameter is required for continuous publishing and required only once (Azure remembers token).
 az appservice web source-control config --name $webappname --resource-group $webappname \
---repo-url $gitrepo --branch master
+--repo-url $gitrepo --branch master --git-token $token
 
 # Browse to the web app.
 az appservice web browse --name $webappname --resource-group $webappname
