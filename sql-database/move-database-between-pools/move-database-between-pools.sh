d160613758e2157f6b7dab59acf8c302fd939b33
Jan Engelsberg <janeng@microsoft.com>
2017-02-22 15:12:31 -0800

    fixed bug and incorporated feedback

--- sql-database/move-database-between-pools/move-database-between-pools.sh
+++ sql-database/move-database-between-pools/move-database-between-pools.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
+# Set an admin login and password for your database
+adminlogin=ServerAdmin
+password=
 # The logical server name has to be unique in the system
 servername=server-$RANDOM
-
-# Login to Azure
-# az login
 
 # Create a resource group
 az group create -n SampleResourceGroup -l northcentralus
 
 # Create a logical server in the resource group
 az sql server create -n $servername -g SampleResourceGroup -l northcentralus \
-	--administrator-login ServerAdmin --administrator-login-password ASecureP@ssw0rd
+	--administrator-login $adminlogin --administrator-login-password $password
 
 # Create two pools in the logical server
 az sql elastic-pools create -g SampleResourceGroup -l northcentralus --server-name $servername \
 	-n MyFirstPool --dtu 50 --database-dtu-max 20
 az sql elastic-pools create -g SampleResourceGroup -l northcentralus --server-name $servername \
-	-n MySecondPool --dtu 50 --database-dtu-max 20
+	-n MySecondPool --dtu 50 --database-dtu-max 50
 
 # Create a database in the first pool
 az sql db create -g SampleResourceGroup -l northcentralus --server-name $servername \
@@ -28,8 +28,5 @@ az sql db create -g SampleResourceGroup -l northcentralus --server-name $servern
 	-n MySampleDatabase --elastic-pool-name MySecondPool
 
 # Move the database to standalone S1 performance level
-az sql db update -g SampleResourceGroup --server-name $servername \
-	-n MySampleDatabase --set requestedServiceObjectiveId=1b1ebd4d-d903-4baa-97f9-4ea675f5e928
-
-# Cleanup the resource group and all resources in it
-# az group delete -n SampleResourceGroup
+az sql db create -g SampleResourceGroup -l northcentralus --server-name $servername \
+	-n MySampleDatabase --requested-service-objective-name S1
--- sql-database/scale-pool/scale-pool.sh
+++ sql-database/scale-pool/scale-pool.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
+# Set an admin login and password for your database
+adminlogin=ServerAdmin
+password=
 # the logical server name has to be unique in the system
 servername=server-$RANDOM
-
-# Login to Azure
-# az login
 
 # Create a resource group
 az group create -n SampleResourceGroup -l northcentralus
 
 # Create a server
 az sql server create -n $servername -g SampleResourceGroup -l northcentralus \
-	--administrator-login ServerAdmin --administrator-login-password ASecureP@ssw0rd
+	--administrator-login $adminlogin --administrator-login-password $password
 
 # Create a pool
 az sql elastic-pools create -g SampleResourceGroup -l northcentralus --server-name $servername \
 	-n samplepool --dtu 50 --database-dtu-max 20
 
-# Create a database
+# Create two database in the pool
 az sql db create -g SampleResourceGroup -l northcentralus --server-name $servername \
-	-n MySampleDatabase --elastic-pool-name samplepool
+	-n MyFirstSampleDatabase --elastic-pool-name samplepool
+az sql db create -g SampleResourceGroup -l northcentralus --server-name $servername \
+	-n MySecondSampleDatabase --elastic-pool-name samplepool
 
 # Scale up to the pool to 100 eDTU
 az sql elastic-pools update -g SampleResourceGroup --server-name $servername \
 	-n samplepool --set dtu=100
-
-# Cleanup
-# az group delete -n SampleResourceGroup
