11231ffad43883db13ac5169e914074e02f7afaa
Carl Rabeler <carlrab@microsoft.com>
2017-02-23 12:41:38 -0800

    Update monitor-and-scale-database.sh

--- sql-database/monitor-and-scale-database/monitor-and-scale-database.sh
+++ sql-database/monitor-and-scale-database/monitor-and-scale-database.sh
@@ -2,24 +2,24 @@
 
 # Set an admin login and password for your database
 adminlogin=ServerAdmin
-password=
+password=ChangeYourAdminPassword1
 # the logical server name has to be unique in the system
 servername=server-$RANDOM
 
 # Create a resource group
-az group create -n SampleResourceGroup -l northcentralus
+az group create -n MyResourceGroup -l northcentralus
 
 # Create a server
-az sql server create -n $servername -g SampleResourceGroup -l northcentralus \
+az sql server create -n $servername -g MyResourceGroup -l northcentralus \
 	--administrator-login $adminlogin --administrator-login-password $password
 
 # Create a database
-az sql db create -g SampleResourceGroup -l northcentralus --server-name $servername \
+az sql db create -g MyResourceGroup -l northcentralus --server-name $servername \
 	-n MySampleDatabase --requested-service-objective-name S0
 
 # Monitor database size
-az sql db show-usage -n MySampleDatabase -g SampleResourceGroup --server-name $servername
+az sql db show-usage -n MySampleDatabase -g MyResourceGroup --server-name $servername
 
 # Scale up database to S1 performance level (create command executes update if DB already exists)
-az sql db create -g SampleResourceGroup -l northcentralus --server-name $servername \
+az sql db create -g MyResourceGroup -l northcentralus --server-name $servername \
 	-n MySampleDatabase --requested-service-objective-name S1
