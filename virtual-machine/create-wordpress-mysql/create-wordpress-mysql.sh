e863d4f5e1d529c4162563557bffc19573f26b15
neilpeterson <neilpeterson@outlook.com>
2017-02-22 22:09:31 -0800

    additional whitespace issues - wordpress doc

--- virtual-machine/create-wordpress-mysql/create-wordpress-mysql.sh
+++ virtual-machine/create-wordpress-mysql/create-wordpress-mysql.sh
@@ -5,26 +5,25 @@ az group create --name myResourceGroup --location westus
 
 # Create a VM
 az vm create \
-  --resource-group myResourceGroup \
-  --name myVM \
-  --location westus \
-  --image UbuntuLTS \
-  --ssh-key-value ~/.ssh/id_rsa.pub \
-  --admin-username azureuser
+    --resource-group myResourceGroup \
+    --name myVM \
+    --location westus \
+    --image UbuntuLTS \
+    --ssh-key-value ~/.ssh/id_rsa.pub \
+    --admin-username azureuser
 
-# Start a CustomScript extension to use a simple bash script to update, download and install WordPress and MySQL 
-az vm extension set \
-  --name customscript \
-  --publisher Microsoft.Azure.Extensions \
-  --version 2.0
-  --name CustomScript \
-  --vm-name myVM \
-  --resource-group myResourceGroup \ 
-  --settings '{"fileUris":["https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/wordpress-single-vm-ubuntu/install_wordpress.sh"], "commandToExecute":"sh install_wordpress.sh" }'
-
-  # open port 80 to allow web traffic to host
-  az vm open-port \
+# Open port 80 to allow web traffic to host.
+az vm open-port \
     --port 80 \
     --priority 300 \
     --resource-group myResourceGroup \
     --name myVM
+
+# Start a CustomScript extension to use a simple bash script to update, download and install WordPress and MySQL 
+az vm extension set \
+    --publisher Microsoft.Azure.Extensions \
+    --version 2.0 \
+    --name CustomScript \
+    --vm-name myVM \
+    --resource-group myResourceGroup \
+    --settings '{"fileUris":["https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/wordpress-single-vm-ubuntu/install_wordpress.sh"],"commandToExecute":"sh install_wordpress.sh"}'
