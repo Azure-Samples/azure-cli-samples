e539e87d375a276131ca470b84e76aa2ffae1e5f
neilpeterson <neilpeterson@outlook.com>
2017-02-25 17:09:18 -0800

    embed scripts, switch to nginx

--- virtual-machine/create-vm-nginx/create-vm-nginx.sh
+++ virtual-machine/create-vm-nginx/create-vm-nginx.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Create a resource group.
-az group create --name myResourceGroup --location westeurope
+az group create --name myResourceGroup12 --location westeurope
 
 # Create a virtual machine. 
 az vm create \
   --image UbuntuLTS \
   --admin-username azureuser \
   --ssh-key-value ~/.ssh/id_rsa.pub \
-  --resource-group myResourceGroup \
+  --resource-group myResourceGroup12 \
   --location westeurope \
   --name myVM
 
@@ -16,14 +16,14 @@ az vm create \
   az vm open-port \
     --port 80 \
     --priority 300 \
-    --resource-group myResourceGroup \
+    --resource-group myResourceGroup12 \
     --name myVM
 
 # Use CustomScript extension to install Apache.
 az vm extension set \
   --publisher Microsoft.Azure.Extensions \
-  --version 2.0
+  --version 2.0 \
   --name CustomScript \
   --vm-name myVM \
-  --resource-group myResourceGroup \ 
-  --settings '{"fileUris":["https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/apache2-on-ubuntu-vm/install_apache.sh"], "commandToExecute":"sh install_apache.sh" }'
+  --resource-group myResourceGroup12 \
+  --settings '{"commandToExecute":"apt-get -y update && apt-get -y install nginx"}'
