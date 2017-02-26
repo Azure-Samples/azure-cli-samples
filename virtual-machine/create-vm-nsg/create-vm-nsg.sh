5931189fce0b47645a52e3a3229ba74e541062d6
neilpeterson <neilpeterson@outlook.com>
2017-02-25 16:33:10 -0800

    simplified script

--- virtual-machine/create-vm-nsg/create-vm-nsg.sh
+++ virtual-machine/create-vm-nsg/create-vm-nsg.sh
@@ -1,69 +1,46 @@
 #!/bin/bash
 
 # Create a resource group.
-az group create --name myResourceGroup --location westeurope
+az group create --name myResourceGroup6 --location westeurope
 
 # Create a virtual network and subnet (front end).
-az network vnet create --resource-group myResourceGroup --name myVnet \
+az network vnet create --resource-group myResourceGroup6 --name myVnet \
   --address-prefix 192.168.0.0/16 --subnet-name mySubnetFrontEnd --subnet-prefix 192.168.1.0/24
 
 # Create a subnet (back end) and associate with virtual network. 
-az network vnet subnet create --resource-group myResourceGroup --vnet-name myVnet \
+az network vnet subnet create --resource-group myResourceGroup6 --vnet-name myVnet \
   --name mySubnetBackEnd --address-prefix 192.168.2.0/24
 
-# Create a public IP address.
-az network public-ip create --resource-group myResourceGroup --name myPublicIP
+# Create a virtual machine. 
+az vm create \
+  --resource-group myResourceGroup6 \
+  --name myVMFrontEnd \
+  --image UbuntuLTS \
+  --vnet-name myVnet \
+  --subnet mySubnetFrontEnd \
+  --nsg myNetworkSecurityGroupFrontEnd
 
-# Create a network security group.
-az network nsg create --resource-group myResourceGroup --name myNetworkSecurityGroupFrontEnd
+# Create a virtual machine without a public IP address.
+az vm create \
+  --resource-group myResourceGroup6 \
+  --name myVMBackEnd \
+  --image UbuntuLTS \
+  --vnet-name myVnet \
+  --subnet mySubnetBackEnd \
+  --nsg myNetworkSecurityGroupBackEnd \
+  --public-ip-address ""
 
-# Create an inbound network security group rule for port 22.
-az network nsg rule create --resource-group myResourceGroup \
-  --nsg-name myNetworkSecurityGroupFrontEnd --name myNetworkSecurityGroupRuleFrontEndSSH \
-  --protocol tcp --direction inbound --priority 1000 --source-address-prefix '*' \
-  --source-port-range '*' --destination-address-prefix '*' --destination-port-range 22 \
-  --access allow
-
-# Associate network security group with subnet. 
-az network vnet subnet update --resource-group myResourceGroup --vnet-name myVnet \
-  --name mySubnetFrontEnd --network-security-group myNetworkSecurityGroupFrontEnd
-
-# Create a network security group.
-az network nsg create --resource-group myResourceGroup --name myNetworkSecurityGroupBackEnd
-
-# Create an inbound network security group rule for port 22.
-az network nsg rule create --resource-group myResourceGroup \
-  --nsg-name myNetworkSecurityGroupBackEnd --name myNetworkSecurityGroupRuleBackEndSSH \
+# Update backend network security group rule to limit source prefix.
+az network nsg rule update --resource-group myResourceGroup6 \
+  --nsg-name myNetworkSecurityGroupBackEnd --name default-allow-ssh \
   --protocol tcp --direction inbound --priority 1000 --source-address-prefix 192.168.1.0/24 \
   --source-port-range '*' --destination-address-prefix '*' --destination-port-range 22 \
   --access allow
 
 # Associate network security group with subnet. 
-az network vnet subnet update --resource-group myResourceGroup --vnet-name myVnet \
+az network vnet subnet update --resource-group myResourceGroup6 --vnet-name myVnet \
   --name mySubnetBackEnd --network-security-group myNetworkSecurityGroupBackEnd
 
-# Create a virtual network card and associate with front end subnet and public IP address.
-az network nic create --resource-group myResourceGroup --name myNic1 \
-    --vnet-name myVnet --subnet mySubnetFrontEnd --public-ip-address myPublicIP
-
-# Create a virtual network card and associate with back end subnet.
-az network nic create --resource-group myResourceGroup --name myNic2 \
-    --vnet-name myVnet --subnet mySubnetBackEnd
-
-# Create a virtual machine. 
-az vm create \
-    --resource-group myResourceGroup \
-    --name myVMFrontEnd \
-    --nics myNic1 \
-    --image UbuntuLTS \
-    --ssh-key-value ~/.ssh/id_rsa.pub \
-    --admin-username azureuser \
-    --no-wait
-
-# Create a virtual machine. 
-az vm create \
-    --resource-group myResourceGroup \
-    --name myVMBackEnd \
-    --nics myNic2\
-    --image UbuntuLTS \
-    --no-wait
+# Associate network security group with subnet. 
+az network vnet subnet update --resource-group myResourceGroup6 --vnet-name myVnet \
+  --name mySubnetFrontEnd --network-security-group myNetworkSecurityGroupFrontEnd
