4e069db97d81826c167e246266cb3575ca5597ae
neilpeterson <neilpeterson@outlook.com>
2017-02-26 09:25:30 -0800

    script updates

--- virtual-machine/create-vm-quick/create-vm-quick.sh
+++ virtual-machine/create-vm-quick/create-vm-quick.sh
@@ -4,11 +4,4 @@
 az group create --name myResourceGroup --location westeurope
 
 # Create a virtual machine. 
-az vm create \
-  --image UbuntuLTS \
-  --admin-username azureuser \
-  --ssh-key-value ~/.ssh/id_rsa.pub \
-  --resource-group myResourceGroup \
-  --location westeurope \
-  --name myVM \
-  --no-wait
+az vm create --resource-group myResourceGroup --name myVM --image UbuntuLTS --generate-ssh-keys
