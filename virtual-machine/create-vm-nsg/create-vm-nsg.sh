ed328bf7f5f028a9403b94bf600f3a3be65107d7
neilpeterson <neilpeterson@outlook.com>
2017-02-26 10:22:43 -0800

    renamed resource group

--- virtual-machine/create-vm-nsg/create-vm-nsg.sh
+++ virtual-machine/create-vm-nsg/create-vm-nsg.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
 # Create a resource group.
-az group create --name myResourceGroup5 --location westeurope
+az group create --name myResourceGroup --location westeurope
 
 # Create a virtual network and subnet (front end).
-az network vnet create --resource-group myResourceGroup5 --name myVnet --address-prefix 192.168.0.0/16 \
+az network vnet create --resource-group myResourceGroup --name myVnet --address-prefix 192.168.0.0/16 \
 --subnet-name mySubnetFrontEnd --subnet-prefix 192.168.1.0/24
 
 # Create a subnet (back end) and associate with virtual network. 
-az network vnet subnet create --resource-group myResourceGroup5 --vnet-name myVnet \
+az network vnet subnet create --resource-group myResourceGroup --vnet-name myVnet \
   --name mySubnetBackEnd --address-prefix 192.168.2.0/24
 
 # Create a virtual machine. 
-az vm create --resource-group myResourceGroup5 --name myVMFrontEnd --image UbuntuLTS \
+az vm create --resource-group myResourceGroup --name myVMFrontEnd --image UbuntuLTS \
   --vnet-name myVnet --subnet mySubnetFrontEnd --nsg myNetworkSecurityGroupFrontEnd --generate-ssh-keys
 
 # Create a virtual machine without a public IP address.
-az vm create --resource-group myResourceGroup5 --name myVMBackEnd --image UbuntuLTS \
+az vm create --resource-group myResourceGroup --name myVMBackEnd --image UbuntuLTS \
   --vnet-name myVnet --subnet mySubnetBackEnd --nsg myNetworkSecurityGroupBackEnd \
   --public-ip-address "" --generate-ssh-keys
 
 # Update backend network security group rule to limit source prefix.
-az network nsg rule update --resource-group myResourceGroup5 --nsg-name myNetworkSecurityGroupBackEnd \
+az network nsg rule update --resource-group myResourceGroup --nsg-name myNetworkSecurityGroupBackEnd \
   --name default-allow-ssh --protocol tcp --direction inbound --priority 1000 \
   --source-address-prefix 192.168.1.0/24 --source-port-range '*' --destination-address-prefix '*' \
   --destination-port-range 22 --access allow
 
 # Associate network security group with subnet. 
-az network vnet subnet update --resource-group myResourceGroup5 --vnet-name myVnet \
+az network vnet subnet update --resource-group myResourceGroup --vnet-name myVnet \
   --name mySubnetBackEnd --network-security-group myNetworkSecurityGroupBackEnd
 
 # Associate network security group with subnet. 
-az network vnet subnet update --resource-group myResourceGroup5 --vnet-name myVnet \
+az network vnet subnet update --resource-group myResourceGroup --vnet-name myVnet \
   --name mySubnetFrontEnd  --network-security-group myNetworkSecurityGroupFrontEnd
