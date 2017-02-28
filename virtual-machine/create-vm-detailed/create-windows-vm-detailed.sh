a0eb2e99acd7906773a6e6dbba99e4d82115d02f
neilpeterson <neilpeterson@outlook.com>
2017-02-27 22:50:08 -0800

    Windows CLI updates

--- virtual-machine/create-vm-detailed/create-windows-vm-detailed.sh
+++ virtual-machine/create-vm-detailed/create-windows-vm-detailed.sh
@@ -4,39 +4,35 @@
 AdminPassword=ChangeYourAdminPassword1
 
 # Create a resource group.
-az group create --name myResourceGroup --location westus
+az group create --name myResourceGroup --location westeurope
 
 # Create a virtual network.
-az network vnet create --resource-group myResourceGroup --location westus --name myVnet \
-  --address-prefix 192.168.0.0/16 --subnet-name mySubnet --subnet-prefix 192.168.1.0/24
+az network vnet create --resource-group myResourceGroup --name myVnet --subnet-name mySubnet
 
-# Create a public IP address and specify a DNS name.
-az network public-ip create --resource-group myResourceGroup --location westus \
-  --name myPublicIP --dns-name mypublicdns$RANDOM --allocation-method static --idle-timeout 4
+# Create a public IP address.
+az network public-ip create --resource-group myResourceGroup --name myPublicIP
 
 # Create a network security group.
-az network nsg create --resource-group myResourceGroup --location westus \
-  --name myNetworkSecurityGroup
-
-# Create an inbound network security group rule for port 22.
-az network nsg rule create --resource-group myResourceGroup \
-  --nsg-name myNetworkSecurityGroup --name myNetworkSecurityGroupRuleRDC \
-  --protocol tcp --direction inbound --priority 1000 --source-address-prefix '*' \
-  --source-port-range '*' --destination-address-prefix '*' --destination-port-range 3389 \
-  --access allow
+az network nsg create --resource-group myResourceGroup --name myNetworkSecurityGroup
 
 # Create a virtual network card and associate with public IP address and NSG.
-az network nic create --resource-group myResourceGroup --location westus --name myNic1 \
-  --vnet-name myVnet --subnet mySubnet --network-security-group myNetworkSecurityGroup \
+az network nic create \
+  --resource-group myResourceGroup \
+  --name myNic \
+  --vnet-name myVnet \
+  --subnet mySubnet \
+  --network-security-group myNetworkSecurityGroup \
   --public-ip-address myPublicIP
 
 # Create a virtual machine. 
 az vm create \
     --resource-group myResourceGroup \
-    --name myVM1 \
-    --location westus \
-    --nics myNic1 \
+    --name myVM \
+    --location westeurope \
+    --nics myNic \
     --image win2016datacenter \
     --admin-username azureuser \
-    --admin-password $AdminPassword \
-    --no-wait
+    --admin-password $AdminPassword
+
+# Open port 3389 to allow RDP traffic to host.
+az vm open-port --port 3389 --resource-group myResourceGroup --name myVM
