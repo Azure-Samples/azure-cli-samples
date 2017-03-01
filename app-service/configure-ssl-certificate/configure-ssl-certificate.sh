d5eaba5483f417ae903701b99ca9ac2fc2b91d3d
Cephas Lin <cephalin@microsoft.com>
2017-03-01 15:53:40 +0100

    added code for custom domain

--- app-service/configure-ssl-certificate/configure-ssl-certificate.sh
+++ app-service/configure-ssl-certificate/configure-ssl-certificate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+fqdn=<Replace with www.{yourdomain}>
 pfxPath=<Replace with path to your .PFX file>
 pfxPassword=<Replace with your .PFX password>
 webappname=mywebapp$RANDOM
@@ -14,12 +15,17 @@ az appservice plan create --name $webappname --resource-group myResourceGroup --
 az appservice web create --name $webappname --resource-group myResourceGroup \
 --plan $webappname
 
+# Map your prepared custom domain name to the web app.
+az appservice web config hostname add --webapp $webappname --resource-group myResourceGroup \
+--name $fqdn
+
 # Upload the SSL certificate and get the thumbprint.
 thumprint=$(az appservice web config ssl upload --certificate-file $pfxPath \
 --certificate-password $pfxPassword --name $webappname --resource-group myResourceGroup \
 --query thumbprint --output tsv)
 
+# Binds the uploaded SSL certificate to the web app.
 az appservice web config ssl bind --certificate-thumbprint $thumbprint --ssl-type SNI \
 --name $webappname --resource-group myResourceGroup
 
-echo "You can now browse to https://<your-app's-domain-name>"
+echo "You can now browse to https://$fqdn"
