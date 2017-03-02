39b6de93ec5960a4bd8b4793bba602750e63fa35
Cory Fowler <cfowler@microsoft.com>
2017-03-02 10:52:55 +0000

    mistyped the variable name

--- app-service/deploy-linux-acr/deploy-linux-acr.sh
+++ app-service/deploy-linux-acr/deploy-linux-acr.sh
@@ -14,10 +14,10 @@ az group create --name myResourceGroup --location $location
 acrValues=$(az acr create --name $appName --resource-group myResourceGroup --location $location --admin-enabled true --query "[loginServer,username,password]" --output tsv | cut -f1234)
 
 # Pull from Docker
-docker login ${acrValue[0]} -u ${acrValue[1]} -p ${acrValue[2]}
+docker login ${acrValues[0]} -u ${acrValues[1]} -p ${acrValues[2]}
 docker pull $dockerContainerUser/$dockerHubContainerName:$dockerContainerVersion
-docker tag $dockerContainerUser/$dockerHubContainerName:$dockerContainerVersion ${acrValue[0]}/$dockerHubContainerName:$dockerContainerVersion
-docker push ${acrValue[0]}/$dockerHubContainerName:$dockerContainerVersion
+docker tag $dockerContainerUser/$dockerHubContainerName:$dockerContainerVersion ${acrValues[0]}/$dockerHubContainerName:$dockerContainerVersion
+docker push ${acrValues[0]}/$dockerHubContainerName:$dockerContainerVersion
 
 # Create an App Service Plan
 az appservice plan create --name AppServiceLinuxDockerPlan --resource-group myResourceGroup --location $location --is-linux --sku S1
@@ -26,4 +26,4 @@ az appservice plan create --name AppServiceLinuxDockerPlan --resource-group myRe
 az appservice web create --name $appName --plan AppServiceLinuxDockerPlan --resource-group myResourceGroup
 
 # Configure Web App with a Custom Docker Container from Docker Hub
-az appservice web config container update --docker-registry-server-url http://${acrValue[0]} --docker-custom-image-name ${acrValue[0]}/$dockerHubContainerName:$dockerContainerVersion --docker-registry-server-user ${acrValue[1]} --docker-registry-server-password ${acrValue[2]} --name $appName --resource-group myResourceGroup
+az appservice web config container update --docker-registry-server-url http://${acrValues[0]} --docker-custom-image-name ${acrValues[0]}/$dockerHubContainerName:$dockerContainerVersion --docker-registry-server-user ${acrValues[1]} --docker-registry-server-password ${acrValues[2]} --name $appName --resource-group myResourceGroup
