8f1350d8a1507ba58716df1aec474ea304bd3b53
Cory Fowler <cfowler@microsoft.com>
2017-03-03 10:03:18 +0000

    redis and docdb working

--- app-service/connect-to-documentdb/connect-to-documentdb.sh
+++ app-service/connect-to-documentdb/connect-to-documentdb.sh
@@ -1,21 +1,22 @@
 #/bin/bash
 
 # Variables
-appName="webappwithredis$random"
-storageName="webappredis$random"
+appName="webappwithdocumentdb$random"
+storageName="webappwithdocumentdb$random"
 location="WestUS"
 
 # Create a Resource Group 
 az group create --name myResourceGroup --location $location
 
 # Create an App Service Plan
-az appservice plan create --name WebAppWithStoragePlan --resource-group myResourceGroup --location $location
+az appservice plan create --name WebAppWithDocumentDBPlan --resource-group myResourceGroup --location $location
 
 # Create a Web App
-az appservice web create --name $appName --plan WebAppWithStoragePlan --resource-group myResourceGroup 
+az appservice web create --name $appName --plan WebAppWithDocumentDBPlan --resource-group myResourceGroup 
 
 # Create a DocumentDB
-docdb=($(az documentdb create --name $appName --resource-group myResourceGroup --query hostName,accessKeys.primaryKey --output tsv))
+docdb=$(az documentdb create --name $appName --resource-group myResourceGroup --query documentEndpoint --output tsv)
+docCreds=$(az documentdb list-keys --name $appName --resource-group myResourceGroup --query primaryMasterKey --output tsv)
 
 # Assign the connection string to an App Setting in the Web App
-az appservice web config appsettings update --settings "DOCDB_URL=${docdb[0]} DOCDB_KEY=${docdb[1]}" --name $appName --resource-group myResourceGroup
+az appservice web config appsettings update --settings "DOCDB_URL=$docdb" "DOCDB_KEY=$docCreds" --name $appName --resource-group myResourceGroup
--- app-service/connect-to-redis/connect-to-redis.sh
+++ app-service/connect-to-redis/connect-to-redis.sh
@@ -1,24 +1,22 @@
 #/bin/bash
 
 # Variables
-appName="webappwithredis$random"
-storageName="webappredis$random"
+resourceGroupName="myResourceGroup$RANDOM"
+appName="webappwithredis$RANDOM"
+storageName="webappredis$RANDOM"
 location="westeurope"
 
 # Create a Resource Group 
-az group create --name myResourceGroup --location $location
+az group create --name $resourceGroupName --location $location
 
 # Create an App Service Plan
-az appservice plan create --name WebAppWithRedisPlan --resource-group myResourceGroup --location $location
+az appservice plan create --name WebAppWithRedisPlan --resource-group $resourceGroupName --location $location
 
 # Create a Web App
-az appservice web create --name $appName --plan WebAppWithRedisPlan --resource-group myResourceGroup 
+az appservice web create --name $appName --plan WebAppWithRedisPlan --resource-group $resourceGroupName 
 
 # Create a Redis Cache
-redis=($(az redis create --name $appName --resource-group myResourceGroup --location $location --sku-capacity 0 --sku-family C --sku-name Basic --query [hostName,port,sslPort,enableNonSslPort,accessKeys.primaryKey] --output tsv))
-
-# Should Connect over SSL
-redisPort=[${redis[3]} -eq 'false'] && echo ${redis[1]} || echo ${redis[2]}
+redis=($(az redis create --name $appName --resource-group $resourceGroupName --location $location --sku-capacity 0 --sku-family C --sku-name Basic --query [hostName,sslPort,accessKeys.primaryKey] --output tsv))
 
 # Assign the connection string to an App Setting in the Web App
-az appservice web config appsettings update --settings "REDIS_URL=${redis[0]}:${redisPort} REDIS_KEY=${redis[4]}" --name $appName --resource-group myResourceGroup
+az appservice web config appsettings update --settings "REDIS_URL=${redis[0]}" "REDIS_PORT=${redis[1]}" "REDIS_KEY=${redis[2]}" --name $appName --resource-group $resourceGroupName
