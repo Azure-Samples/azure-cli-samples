3b4dbcb5e5e3b457e71feb29116a0847d8234af0
Allen Clark (VS) <allclark@microsoft.com>
2017-03-08 16:01:27 -0800

    Simplify the script

--- virtual-machine/restart-by-tag/restart-by-tag.sh
+++ virtual-machine/restart-by-tag/restart-by-tag.sh
@@ -1,65 +1,15 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/bash
 
-DEFAULT_TAG='restart-tag'
-LOCATION='westus'
-TAG=${1:-$DEFAULT_TAG}
+# Create a resource group where we'll create the VMs that we'll start
+az group create -n myResourceGroup -l westus
 
-RESOURCE_GROUPS=('GROUP1' 'GROUP2' 'GROUP3')
+# Create the VMs. Two are tagged and one is not. --generated-ssh-keys will create ssh keys if not present
+az vm create -g myResourceGroup -n myVM1 --image UbuntuLTS --admin-username deploy --tags "restart-tag" --generate-ssh-keys
+az vm create -g myResourceGroup -n myVM2 --image UbuntuLTS --admin-username deploy --tags "restart-tag"
+az vm create -g myResourceGroup -n myVM3 --image UbuntuLTS --admin-username deploy
 
-for group in ${RESOURCE_GROUPS[@]}; do
-    # Create the resource group if it doesn't exist
-    echo "Creating resource group ${group} in ${LOCATION}"
-    az group create -n ${group} -l ${LOCATION} 1>/dev/null
+# Get the IDs of all the VMs in the resource group and restart those
+az vm restart --ids $(az vm list --query "join(' ', [?resourceGroup=="myResourceGroup"] | [].id)" -o tsv)
 
-    echo "Deploying vm named ${group}-vm in ${group} with no waiting"
-    az vm create -g ${group} -n "${group}-vm" --image UbuntuLTS --admin-username deploy --tags ${DEFAULT_TAG} --no-wait $1>/dev/null
-    # If you don't have an ssh key, you can add the --generate-ssh-keys parameter to the az vm create command
-    # az vm create -g ${group} -n "${group}-vm" --image UbuntuLTS --admin-username deploy --tags ${DEFAULT_TAG} --generate-ssh-keys --no-wait $1>/dev/null
-done
-
-echo "Waiting for the vms to complete provisioning"
-
-GROUP_QUERY=''
-for group in ${RESOURCE_GROUPS[@]}; do
-    if [[ ${GROUP_QUERY} ]]; then
-        GROUP_QUERY="${GROUP_QUERY} || resourceGroup=='${group}'"
-    else
-        GROUP_QUERY="[?resourceGroup=='${group}'"
-    fi
-done
-
-SUCCESS_GROUP_QUERY="length(${GROUP_QUERY}] | [?provisioningState=='Succeeded'])"
-FAILED_GROUP_QUERY="length(${GROUP_QUERY}] | [?provisioningState=='Failed'])"
-
-echo ""
-while [[ $(az vm list --query "${SUCCESS_GROUP_QUERY}") != ${#RESOURCE_GROUPS[@]} ]]; do
-    echo "Still not provisioned. Sleeping for 20 seconds."
-    sleep 20
-    if [[ $(az vm list --query "${FAILED_GROUP_QUERY}") != 0 ]]; then
-        echo "At least one of the vms failed to provision successfully!!"
-        exit 1
-    fi
-done
-
-echo ""
-echo "Restarting virtual machines with ids via the group query"
-az vm restart --ids $(az vm list --query "join(' ', ${GROUP_QUERY}] | [].id)" -o tsv) $1>/dev/null
-
-echo ""
-echo "Restarting virtual machines with a given tag"
-az vm restart --ids $(az resource list --tag ${TAG} --query "[?type=='Microsoft.Compute/virtualMachines'].id" -o tsv) $1>/dev/null
-
-
-echo ""
-echo "To delete the created resource groups run the following."
-DELETE_CMD=''
-for group in ${RESOURCE_GROUPS[@]}; do
-    if [[ ${DELETE_CMD} ]]; then
-        DELETE_CMD="${DELETE_CMD} && az group delete -n ${group} --no-wait --yes"
-    else
-        DELETE_CMD="az group delete -n ${group} --no-wait --yes"
-    fi
-done
-
-echo "'${DELETE_CMD}'"
+# Use the tags to get the IDS of VMs and restart those
+az vm restart --ids $(az resource list --tag "restart-tag" --query "[?type=='Microsoft.Compute/virtualMachines'].id" -o tsv)
