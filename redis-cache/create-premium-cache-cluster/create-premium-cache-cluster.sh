990f9797df016d6dc1de0a3e6e3bb0b32e6b99d9
Steve Danielson <sdanie@microsoft.com>
2017-04-05 16:32:56 -0400

    Redis Cache CLI - removed location variable

--- redis-cache/create-premium-cache-cluster/create-premium-cache-cluster.sh
+++ redis-cache/create-premium-cache-cluster/create-premium-cache-cluster.sh
@@ -3,11 +3,10 @@
 # Variables
 resourceGroupName="myResourceGroup$RANDOM"
 cacheName="myCache$RANDOM"
-location="eastus"
 
 # Create a Resource Group 
 az group create --name $resourceGroupName --location $location
 
 # Create a Redis Cache
-az redis create --name $cacheName --resource-group $resourceGroupName --location $location --sku-capacity 1 --sku-family P --sku-name Premium --shard-count 2
+az redis create --name $cacheName --resource-group $resourceGroupName --location eastus --sku-capacity 1 --sku-family P --sku-name Premium --shard-count 2
 
