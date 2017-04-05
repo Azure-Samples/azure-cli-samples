012ef73381f6642272e18aa7998e5cccd845e12c
Cory Fowler <cfowler@microsoft.com>
2017-04-04 18:40:32 -0700

    adding first creation scripts

---
+++ azure-functions/create-function-app-consumption/create-function-app-consumption.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Create resource group
+az group create --name myResourceGroup --location westeurope
+
+# Create an azure storage account
+az storage account create --name $storageName --location westeurope --resource-group myResourceGroup
+
+# Create Function App
+az functionapp create --name myFunctionApp --storage-account $storageName --consumption --resource-group myResourceGroup
