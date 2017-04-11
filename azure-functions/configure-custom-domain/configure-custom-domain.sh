07de5fa3768e3a9c863ccadd45ee52309cc087dc
Glenn Gailey <ggailey777@hotmail.com>
2017-04-10 17:00:27 -0700

    Fix for A record only custom domains

--- azure-functions/configure-custom-domain/configure-custom-domain.sh
+++ azure-functions/configure-custom-domain/configure-custom-domain.sh
@@ -19,12 +19,12 @@ az appservice plan create --name FunctionAppWithAppServicePlan \
 az functionapp create --name $functionAppName --storage-account $storageName \
 --plan FunctionAppWithAppServicePlan --resource-group myResourceGroup
 
-echo "Configure a CNAME record that maps $fqdn to $functionAppName.azurewebsites.net"
+echo "Configure an A record that maps $fqdn to $functionAppName.azurewebsites.net"
 read -p "Press [Enter] key when ready ..."
 
 # Before continuing, go to your DNS configuration UI for your custom domain and follow the 
-# instructions at https://aka.ms/appservicecustomdns to configure a CNAME record for the 
-# hostname "www" and point it your web app's default domain name.
+# instructions at https://aka.ms/appservicecustomdns to configure an A record 
+# and point it your web app's default domain name.
 
 # Map your prepared custom domain name to the function app.
 az appservice web config hostname add --webapp $functionAppName --resource-group myResourceGroup \
