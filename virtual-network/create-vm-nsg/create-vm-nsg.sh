195a8af98f69641597f7d114f5d750d703c1a352
Mark Brown <markjbrown@users.noreply.github.com>
2017-04-19 14:38:08 -0700

    Clean up naming and 10. addresses

--- virtual-network/create-vm-nsg/create-vm-nsg.sh
+++ virtual-network/create-vm-nsg/create-vm-nsg.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
+$RgName=MyResourceGroup
+
 # Create a resource group.
 az group create --name myResourceGroup --location westeurope
 
 # Create a virtual network and subnet (front end).
-az network vnet create --resource-group myResourceGroup --name myVnet --address-prefix 192.168.0.0/16 \
---subnet-name mySubnetFrontEnd --subnet-prefix 192.168.1.0/24
+az network vnet create --resource-group $RgName --name MyVnet --address-prefix 10.0.0.0/16 \
+--subnet-name MySubnetFrontEnd --subnet-prefix 10.0.1.0/24
 
 # Create a subnet (back end) and associate with virtual network. 
-az network vnet subnet create --resource-group myResourceGroup --vnet-name myVnet \
-  --name mySubnetBackEnd --address-prefix 192.168.2.0/24
+az network vnet subnet create --resource-group $RgName --vnet-name MyVnet \
+  --name MySubnetBackEnd --address-prefix 10.0.2.0/24
 
 # Create a new virtual machine, this creates SSH keys if not present.
-az vm create --resource-group myResourceGroup --name myVMFrontEnd --image UbuntuLTS \
-  --vnet-name myVnet --subnet mySubnetFrontEnd --nsg myNetworkSecurityGroupFrontEnd \
+az vm create --resource-group $RgName --name MyVMFrontEnd --image UbuntuLTS \
+  --vnet-name MyVnet --subnet MySubnetFrontEnd --nsg MyNetworkSecurityGroupFrontEnd \
   --generate-ssh-keys --no-wait
 
 # Create a virtual machine without a public IP address.
-az vm create --resource-group myResourceGroup --name myVMBackEnd --image UbuntuLTS \
-  --vnet-name myVnet --subnet mySubnetBackEnd --nsg myNetworkSecurityGroupBackEnd \
+az vm create --resource-group $RgName --name MyVMBackEnd --image UbuntuLTS \
+  --vnet-name MyVnet --subnet MySubnetBackEnd --nsg MyNetworkSecurityGroupBackEnd \
   --public-ip-address "" --generate-ssh-keys
 
 # Get nsg rule name.
-nsgrule=$(az network nsg rule list --resource-group myResourceGroup --nsg-name myNetworkSecurityGroupBackEnd --query [0].name -o tsv)
+nsgrule=$(az network nsg rule list --resource-group $RgName --nsg-name MyNetworkSecurityGroupBackEnd --query [0].name -o tsv)
 
 # Update backend network security group rule to limit source prefix.
-az network nsg rule update --resource-group myResourceGroup --nsg-name myNetworkSecurityGroupBackEnd \
+az network nsg rule update --resource-group $RgName --nsg-name MyNetworkSecurityGroupBackEnd \
   --name $nsgrule --protocol tcp --direction inbound --priority 1000 \
-  --source-address-prefix 192.168.1.0/24 --source-port-range '*' --destination-address-prefix '*' \
+  --source-address-prefix 10.0.1.0/24 --source-port-range '*' --destination-address-prefix '*' \
   --destination-port-range 22 --access allow
