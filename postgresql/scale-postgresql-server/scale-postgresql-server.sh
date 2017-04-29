02070cbab96601eeffd82e1e857bad46d2ad70a9
Jason H <jasonh@microsoft.com>
2017-04-28 18:22:48 -0700

    adding new samples for release at //build conference

---
+++ postgresql/scale-postgresql-server/scale-postgresql-server.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Set an admin login and password for your database
+adminlogin=ServerAdmin
+password=ChangeYourAdminPassword1
+
+# the logical server name has to be unique in the system
+servername=server-$RANDOM
+
+# Create a resource group
+az group create \
+--name myResourceGroup \
+--location westus
+
+# Create a PostgreSQL server in the resource group
+az postgres server create \
+--name $servername \
+--resource-group myResourceGroup \
+--location westus \
+--admin-user $adminlogin \
+--admin-password $password \
+--performance-tier Standard \
+--compute-units 100 \
+
+# Monitor usage metrics
+<need to update>
+
+# Scale up the server to provision more Compute Units
+az postgres server update \
+--resource-group myResourceGroup \
+--name $servername \
+--compute-units 400
