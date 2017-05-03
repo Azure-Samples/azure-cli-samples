9ec049b615528493a4be1a8a10fe70da6717a081
Tamra Myers <tamram@microsoft.com>
2017-05-02 17:28:30 -0700

    minor fixes to scripts

--- batch/run-job/run-job.sh
+++ batch/run-job/run-job.sh
@@ -13,7 +13,7 @@ az batch job create --id myjob --pool-id mypool
 # more information see the sample script for adding applications.
 az batch task create \
     --job-id myjob \
-    --id task1 \
+    --task-id task1 \
     --application-package-references myapp#1.0
     --command-line "cmd /c %AZ_BATCH_APP_PACKAGE_MYAPP#1.0%\\myapp.exe"
 
@@ -23,10 +23,10 @@ az batch task create --job-id myjob --json-file tasks.json
 
 # Now that all the tasks are added - we can update the job so that it will automatically
 # be marked as completed once all the tasks are finished.
-az batch job set --on-all-tasks-complete terminateJob
+az batch job set --job-id myjob --on-all-tasks-complete terminateJob
 
 # Monitor the status of the job.
 az batch job show --job-id myjob
 
 # Monitor the status of a task.
-az batch task show --task-id task1
+az batch task show --job-id myjob --task-id task1
