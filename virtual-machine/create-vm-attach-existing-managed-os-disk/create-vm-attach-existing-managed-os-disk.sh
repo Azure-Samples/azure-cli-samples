542f7b568415d66bc3bfa14843a85290c12fba5f
ramankumarlive <ramankum@microsoft.com>
2017-05-12 17:55:26 -0700

    updated to use managed disk id

--- virtual-machine/create-vm-attach-existing-managed-os-disk/create-vm-attach-existing-managed-os-disk.sh
+++ virtual-machine/create-vm-attach-existing-managed-os-disk/create-vm-attach-existing-managed-os-disk.sh
@@ -5,19 +5,22 @@ subscriptionId=6492b1f7-f219-446b-b509-314e17e1efb0
 resourceGroupName=myResourceGroupName
 
 #Provide the name of the Managed Disk
-manageddiskName=myOSDiskName
+managedDiskName=myDiskName
 
 #Provide the OS type
 osType=linux
 
 #Provide the name of the virtual machine
-virtualMachineName=myVirtualMachineName
+virtualMachineName=myVirtualMachineName123
 
 #Set the context to the subscription Id where Managed Disk exists and where VM will be created
 az account set --subscription $subscriptionId
 
-#Create VM by attaching existing managed disks as OS
-#if managed disk exist in a different resource group then use managed disk Id instead of name of the disk 
-#managedDiskId=$(az disk show --name managedDiskName --resource-group sourceResourceGroupName --query [id] -o tsv)
+#Get the resource Id of the managed disk
+managedDiskId=$(az disk show --name $managedDiskName --resource-group $resourceGroupName --query [id] -o tsv)
 
-az vm create --name $virtualMachineName --resource-group $resourceGroupName --attach-os-disk $manageddiskName --os-type $osType
+#Create VM by attaching existing managed disks as OS
+az vm create --name $virtualMachineName --resource-group $resourceGroupName --attach-os-disk $managedDiskId --os-type $osType
+
+
+
