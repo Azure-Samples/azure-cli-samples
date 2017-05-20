3015795399ba885a6f1d3bdf493f9751e9fce8d5
ramankumarlive <ramankum@microsoft.com>
2017-05-19 17:58:39 -0700

    updated

--- storage/copy-managed-disks-to-same-or-different-subscription/copy-managed-disks-to-same-or-different-subscription.sh
+++ storage/copy-managed-disks-to-same-or-different-subscription/copy-managed-disks-to-same-or-different-subscription.sh
@@ -1,13 +1,13 @@
-#Provide the subscription Id of the subscription where snapshot exists
+#Provide the subscription Id of the subscription where managed disk exists
 sourceSubscriptionId=dd80b94e-0463-4a65-8d04-c94f403879dc
 
-#Provide the name of your resource group where snapshot exists
+#Provide the name of your resource group where managed disk exists
 sourceResourceGroupName=mySourceResourceGroupName
 
-#Provide the name of the snapshot
+#Provide the name of the managed disk
 managedDiskName=myDiskName
 
-#Set the context to the subscription Id where snapshot exists
+#Set the context to the subscription Id where managed disk exists
 az account set --subscription $sourceSubscriptionId
 
 #Get the managed disk Id 
@@ -17,16 +17,14 @@ managedDiskId=$(az disk show --name $managedDiskName --resource-group $sourceRes
 echo 'source managed disk Id is: ' $managedDiskId
 
 #Provide the subscription Id of the subscription where managed disk will be copied to
-#If managed disk is copied to the same subscription then you can skip this step
 targetSubscriptionId=6492b1f7-f219-446b-b509-314e17e1efb0
 
 #Name of the resource group where managed disk will be copied to
 targetResourceGroupName=mytargetResourceGroupName
 
 #Set the context to the subscription Id where managed disk will be copied to
-#If managed disk is copied to the same subscription then you can skip this step
 az account set --subscription $targetSubscriptionId
 
-#Copy managed disk to different subscription using the snapshot Id
+#Copy managed disk to different subscription using managed disk Id
 az disk create --resource-group $targetResourceGroupName --name $managedDiskName --source $managedDiskId
 
