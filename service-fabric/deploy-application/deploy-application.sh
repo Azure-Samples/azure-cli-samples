b7992cd53d3c75256505fdfaad0bd5dc6c9b95f2
samedder <samedder@users.noreply.github.com>
2017-08-21 13:23:30 -0700

    Updating samples to use new sfctl syntax

--- service-fabric/deploy-application/deploy-application.sh
+++ service-fabric/deploy-application/deploy-application.sh
@@ -1,27 +1,24 @@
 ﻿#!/bin/bash
 
-# Authenticate CLI session.
-az login
-
 # Select cluster
-az sf cluster select \
+sfctl cluster select \
     --endpoint http://svcfab1.westus2.cloudapp.azure.com:19080
 
 # Upload the application files to the image store
 # (note the last folder name, Debug in this example)
-az sf application upload \
+sfctl application upload \
     --path  C:\Code\svcfab-vs\svcfab-vs\pkg\Debug \
     --show-progress
 
 # Register the application (manifest files) from the image store
 # (Note the last folder from the previous command is used: Debug)
-az sf application provision \
+sfctl application provision \
     --application-type-build-path Debug \
     --timeout 500
 
 # Create an instance of the registered application and 
 # auto deploy any defined services
-az sf application create \
+sfctl application create \
     --app-name fabric:/MyApp \
     --app-type MyAppType \
     --app-version 1.0.0
