7eec268a2506ae66e00b5b9cef487328013e6939
Iain Foulds <iain.foulds@microsoft.com>
2018-01-02 16:33:13 -0800

    Adding VMSS single AZ script

---
+++ virtual-machine-scale-sets/create-single-availability-zone/create-single-availability-zone.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Create a resource group
+az group create --name myResourceGroup --location eastus2
+
+# Create a scale set in Availability Zone 1
+az vmss create \
+  --resource-group myResourceGroup \
+  --name myScaleSet \
+  --image UbuntuLTS \
+  --upgrade-policy-mode automatic \
+  --admin-username azureuser \
+  --generate-ssh-keys \
+  --zone 1
+
+# Apply the Custom Script Extension that installs a basic Nginx webserver
+az vmss extension set \
+  --publisher Microsoft.Azure.Extensions \
+  --version 2.0 \
+  --name CustomScript \
+  --resource-group myResourceGroup \
+  --vmss-name myScaleSet \
+  --settings '{"fileUris":["https://raw.githubusercontent.com/Azure-Samples/compute-automation-configurations/master/automate_nginx.sh"],"commandToExecute":"./automate_nginx.sh"}'
+
+# Output the public IP address to access the site in a web browser
+az network public-ip show \
+  --resource-group myResourceGroup \
+  --name myScaleSetLBPublicIP \
+  --query [ipAddress] \
+  --output tsv
