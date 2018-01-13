3c68ff5996b7b4060cb67b150a0ba9f62f348bad
Mark Bolz <32652040+bolzmj@users.noreply.github.com>
2018-01-12 16:16:10 -0800

    Updated to create all objects
    Update to create database, VNet, VNet service endpoint and VNet rule.

--- mysql/create-mysql-server-vnet/create-mysql-server.sh
+++ mysql/create-mysql-server-vnet/create-mysql-server.sh
@@ -9,10 +9,48 @@ az group create \
 # Name of a server maps to DNS name and is thus required to be globally unique in Azure.
 # Substitute the <server_admin_password> with your own value.
 az mysql server create \
---name mypgserver-20180112 \
+--name mysqlserver-20180112 \
 --resource-group myresourcegroup \
 --location westus \
 --admin-user mylogin \
 --admin-password <server_admin_password> \
 --performance-tier Standard \
 --compute-units 100
+
+# Get available service endpoints for Azure region
+# Use the command below to get the list of services supported for endpoints, for an Azure region, say "westus".
+az network vnet list-endpoint-services \
+-l westus
+
+# Get available service endpoints for Azure region output is JSON
+# Use the command below to get the list of services supported for endpoints, for an Azure region, say "westus".
+az network vnet list-endpoint-services \
+-l westus
+
+# Add Azure SQL service endpoint to a subnet *mySubnet* while creating the virtual network *myVNet* output is JSON
+az network vnet create \
+-g myRG \
+-n myVNet \
+--address-prefixes 10.0.0.0/16 \
+-l westus
+
+# Creates the service endpoint
+az network vnet subnet create \
+-g myRG \
+-n mySubnet \
+--vnet-name myVNet \
+--address-prefix 10.0.1.0/24 \
+--service-endpoints Microsoft.SQL
+
+# View service endpoints configured on a subnet
+az network vnet subnet show \
+-g myRG \
+-n mySubnet \
+--vnet-name myVNet
+
+# Create a VNet rule on the sever to secure it to the subnet
+az mysql vnet-rule create \
+--name myRule \
+--resource-group myRG \
+--server mysqlserver-20180112 \
+--subnet mySubnet
