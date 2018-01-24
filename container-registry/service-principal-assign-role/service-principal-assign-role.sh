d619b73c40ab68e40419a5e1258ab1e4f3c08f6e
Marsh Macy <marsma@microsoft.com>
2018-01-24 14:43:13 -0800

    [acr] service principal script add

---
+++ container-registry/service-principal-assign-role/service-principal-assign-role.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Modify for your environment. The ACR_NAME is the name of your Azure Container
+# Registry, and the SERVICE_PRINCIPAL_ID is the service principal's 'appId' or
+# one of its 'servicePrincipalNames' values.
+ACR_NAME=mycontainerregistry
+SERVICE_PRINCIPAL_ID=<service-principal-ID>
+
+# Populate value required for subsequent command args
+ACR_REGISTRY_ID=$(az acr show --name $ACR_NAME --query id --output tsv)
+
+# Assign the desired role to the service principal. Modify the '--role' argument
+# value as desired:
+# reader:      pull only
+# contributor: push and pull
+# owner:       push, pull, and assign roles
+az role assignment create --assignee $SERVICE_PRINCIPAL_ID --scope $ACR_REGISTRY_ID --role reader
---
+++ container-registry/service-principal-create/service-principal-create.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Modify for your environment. The ACR_NAME is the name of your Azure Container
+# Registry, and the SERVICE_PRINCIPAL_NAME can be any unique name within your
+# subscription (you can use the default below).
+ACR_NAME=<container-registry-name>
+SERVICE_PRINCIPAL_NAME=acr-service-principal
+
+# Populate some values required for subsequent command args
+ACR_LOGIN_SERVER=$(az acr show --name $ACR_NAME --query loginServer --output tsv)
+ACR_REGISTRY_ID=$(az acr show --name $ACR_NAME --query id --output tsv)
+
+# Create the service principal with rights scoped to the registry.
+# Default permissions are for both docker push and pull access. Modify the
+# '--role' argument value as desired:
+# reader:      pull only
+# contributor: push and pull
+# owner:       push, pull, and assign roles
+SP_PASSWD=$(az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME --scopes $ACR_REGISTRY_ID --role reader --query password --output tsv)
+SP_APP_ID=$(az ad sp show --id http://$SERVICE_PRINCIPAL_NAME --query appId --output tsv)
+
+# Output the service principal's credentials; use these in your services and
+# applications to authenticate to the container registry.
+echo "Service principal ID: $SP_APP_ID"
+echo "Service principal password: $SP_PASSWD"
