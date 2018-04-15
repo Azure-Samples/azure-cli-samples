70e05d9906d5f74e71ea1500219fca0a9c48e357
Julia Kornich <juliako@microsoft.com>
2018-04-15 12:44:22 -0700

    Update media-services-create-account.sh

--- media-services/media-services-create-account/media-services-create-account.sh
+++ media-services/media-services-create-account/media-services-create-account.sh
@@ -2,6 +2,7 @@
 
 resourceGroup=myResourceGroup
 storageName=amsstorename
+amsLocation=westus2
 amsAccountName=amsmediaaccountname
 amsSPName=mediaserviceprincipal
 amsSPPassword=mediasppassword
@@ -9,12 +10,12 @@ amsSPPassword=mediasppassword
 # Create a resource resourceGroupName
 az group create \
   --name $resourceGroup \
-  --location westus2
+  --location $amsLocation
 
 # Create an azure storage account
 az storage account create \
   --name $storageName \
-  --location westus2 \
+  --location $amsLocation \
   --resource-group $resourceGroup
 
 # Create an azure media service account
@@ -22,7 +23,7 @@ az ams account create \
   --name $amsAccountName \
   --resource-group $resourceGroup \
   --storage-account $storageName \
-  --location westus2
+  --location $amsLocation
 
 # Create a service principal with password and configure its access to an Azure Media Services account.
 az ams account sp create \
