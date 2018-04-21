64d308179b22c9223cc46dce4be6c7d687cc2f1e
Wesley McSwain <wesmc@microsoft.com>
2018-04-21 00:24:11 -0400

    update CLI web apps script to add app setting for SignalR

--- azure-signalr/create-signalr-service-and-group/create-signalr-service-and-group.sh
+++ azure-signalr/create-signalr-service-and-group/create-signalr-service-and-group.sh
@@ -13,8 +13,18 @@ myResourceGroupName=$SignalRName"Group"
 az group create --name $myResourceGroupName --location eastus
 
 # Create the Azure SignalR Service resource
-az signalr create \
+signalRresource=$(az signalr create \
   --name $mySignalRSvcName \
   --resource-group $myResourceGroupName \
   --sku Basic_DS2 \
-  --unit-count 1
+  --unit-count 1)
+echo "$signalRresource"
+
+# Get the SignalR primary key 
+signalRkeys=$(az signalr key list --name $mySignalRSvcName --resource-group $myResourceGroupName)
+signalRprimarykey=$(echo "$signalRkeys" | grep -Po '(?<="primaryKey": ")[^"]*')
+
+# Form the connection string for use in your application
+signalRhostname=$(echo "$signalRresource" | grep -Po '(?<="hostName": ")[^"]*')
+connstring="Endpoint=https://$signalRhostname;AccessKey=$signalRprimarykey;"
+echo "$connstring"  
--- azure-signalr/create-signalr-with-app-service/create-signalr-with-app-service.sh
+++ azure-signalr/create-signalr-with-app-service/create-signalr-with-app-service.sh
@@ -9,21 +9,34 @@ SignalRName=SignalRTestSvc$randomNum
 #resource name must be lowercase
 mySignalRSvcName=${SignalRName,,}
 myResourceGroupName=$SignalRName"Group"
-myAppSvcName=SignalRTestWebApp$randomNum
+myWebAppName=SignalRTestWebApp$randomNum
 myAppSvcPlanName=$myAppSvcName"Plan"
 
 # Create resource group 
 az group create --name $myResourceGroupName --location eastus
 
 # Create the Azure SignalR Service resource
-az signalr create \
+signalRresource=$(az signalr create \
   --name $mySignalRSvcName \
   --resource-group $myResourceGroupName \
   --sku Basic_DS2 \
-  --unit-count 1
+  --unit-count 1)
+echo "$signalRresource"
 
 # Create an App Service plan.
 az appservice plan create --name $myAppSvcPlanName --resource-group $myResourceGroupName --sku FREE
 
 # Create the Web App
-az webapp create --name $myAppSvcName --resource-group $myResourceGroupName --plan $myAppSvcPlanName  
+az webapp create --name $myWebAppName --resource-group $myResourceGroupName --plan $myAppSvcPlanName  
+
+# Get the SignalR primary key 
+signalRkeys=$(az signalr key list --name $mySignalRSvcName --resource-group $myResourceGroupName)
+signalRprimarykey=$(echo "$signalRkeys" | grep -Po '(?<="primaryKey": ")[^"]*')
+
+# Form the connection string for use in your application
+signalRhostname=$(echo "$signalRresource" | grep -Po '(?<="hostName": ")[^"]*')
+connstring="Endpoint=https://$signalRhostname;AccessKey=$signalRprimarykey;"
+
+#Add an app setting to the web app for the SignalR connection
+az webapp config appsettings set --name $myWebAppName --resource-group $myResourceGroupName \
+  --settings "SignalRConnectionString=$connstring" 
