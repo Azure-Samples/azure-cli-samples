b7718b2a08719274b1080ef04b5eb4032c0076a4
Mark Bolz <32652040+bolzmj@users.noreply.github.com>
2018-05-23 19:02:03 -0700

    Updating for prod
    Tested in production 5/23/2018

--- postgresql/create-postgresql-server-vnet/create-postgresql-server.sh
+++ postgresql/create-postgresql-server-vnet/create-postgresql-server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#Substitue <subscription id> with your identifyer
+# To find the name of an Azure region in the CLI run this command: az account list-locations
+# Substitute  <subscription id> with your identifier
 az account set --subscription <subscription id>
 
 # Create a resource group
@@ -17,5 +18,38 @@ az postgres server create \
 --location westus \
 --admin-user mylogin \
 --admin-password <server_admin_password> \
---performance-tier Standard \
---compute-units 100
+--sku-name GP_Gen4_2
+
+# Get available service endpoints for Azure region output is JSON
+# Use the command below to get the list of services supported for endpoints, for an Azure region, say "westus".
+az network vnet list-endpoint-services \
+-l westus
+
+# Add Azure SQL service endpoint to a subnet *mySubnet* while creating the virtual network *myVNet* output is JSON
+az network vnet create \
+-g myresourcegroup \
+-n myVNet \
+--address-prefixes 10.0.0.0/16 \
+-l westus
+
+# Creates the service endpoint
+az network vnet subnet create \
+-g myresourcegroup \
+-n mySubnet \
+--vnet-name myVNet \
+--address-prefix 10.0.1.0/24 \
+--service-endpoints Microsoft.SQL
+
+# View service endpoints configured on a subnet
+az network vnet subnet show \
+-g myresourcegroup \
+-n mySubnet \
+--vnet-name myVNet
+
+# Create a VNet rule on the sever to secure it to the subnet. Note: resource group (-g) parameter is where the database exists. VNet resource group if different should be specified using subnet id (URI) instead of subnet, VNet pair.
+az postgres server vnet-rule create \
+-n myRule \
+-g myresourcegroup \
+-s mydemoserver \
+--vnet-name myVNet \
+--subnet mySubnet
