f2d3a9260441edf296672016d1e58eeda8e14724
Mark Brown <markjbrown@users.noreply.github.com>
2018-10-29 20:48:37 -0700

    removed multi-master from firewall example.

--- cosmosdb/secure-cosmosdb-create-firewall/secure-cosmosdb-create-firewall.sh
+++ cosmosdb/secure-cosmosdb-create-firewall/secure-cosmosdb-create-firewall.sh
@@ -13,14 +13,13 @@ az group create \
 	--location $location
 
 
-# Create a SQL API Cosmos DB account with session consistency and multi-master enabled
+# Create a SQL API Cosmos DB account with session consistency
 az cosmosdb create \
     --resource-group $resourceGroupName \
     --name $accountName \
     --kind GlobalDocumentDB \
     --locations "South Central US"=0 "North Central US"=1 \
-    --default-consistency-level "Session" \
-    --enable-multiple-write-locations true
+    --default-consistency-level "Session"
 
 
 # Configure the firewall
