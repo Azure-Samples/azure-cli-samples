ad5a8a390c95a56d7f96750062a17c030ab3d5d1
tfitzmac <tomfitz@microsoft.com>
2018-10-31 15:48:52 -0700

    added examples for preview parameters

--- event-grid/create-custom-topic/create-custom-topic.sh
+++ event-grid/create-custom-topic/create-custom-topic.sh
@@ -3,15 +3,21 @@
 # Give your custom topic a unique name
 myTopic=demoContosoTopic
 
+# Provice name for resource group
+myResourceGroup=demoResourceGroup
+
+# Select the Azure subscription to contain the custom topic.
+az account set --subscription "<name or ID of the subscription>"
+
 # Create resource group
-az group create --name myResourceGroup --location westus2
+az group create --name $myResourceGroup --location westus2
 
 # Create custom topic
-az eventgrid topic create --resource-group myResourceGroup --name $myTopic --location westus2
+az eventgrid topic create --resource-group $myResourceGroup --name $myTopic --location westus2
 
 # Retrieve endpoint and key to use when publishing to the topic
-endpoint=$(az eventgrid topic show --name $myTopic -g myResourceGroup --query "endpoint" --output tsv)
-key=$(az eventgrid topic key list --name $myTopic -g myResourceGroup --query "key1" --output tsv)
+endpoint=$(az eventgrid topic show --name $myTopic -g $myResourceGroup --query "endpoint" --output tsv)
+key=$(az eventgrid topic key list --name $myTopic -g $myResourceGroup --query "key1" --output tsv)
 
 echo $endpoint
 echo $key
--- event-grid/subscribe-to-custom-topic-preview/subscribe-to-custom-topic-preview.sh
+++ event-grid/subscribe-to-custom-topic-preview/subscribe-to-custom-topic-preview.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# You must have the latest version of the Event Grid preview extension.
+# If you have not installed previously:
+# az extension add -n eventgrid
+# If you have installed previously:
+# az extension update -n eventgrid
+
 # Provide the name of the topic you are subscribing to
 myTopic=demoContosoTopic
 
@@ -12,9 +18,11 @@ myEndpoint="<endpoint URL>"
 # Select the Azure subscription that contains the custom topic.
 az account set --subscription "<name or ID of the subscription>"
 
+# Get the resource ID of the custom topic
+topicID=$(az eventgrid topic show --name $myTopic -g $resourceGroup --query id --output tsv)
+
 # Subscribe to the custom event. Include the resource group that contains the custom topic.
 az eventgrid event-subscription create \
-  --resource-group $resourceGroup \
-  --topic-name $myTopic  \
+  --source-resource-id $topicID \
   --name demoSubscription \
   --endpoint $myEndpoint
