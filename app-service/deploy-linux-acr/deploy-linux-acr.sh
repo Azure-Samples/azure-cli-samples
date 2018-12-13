5de239273dd3e28bb2ced5ccd5707a0ca7323cc4
Mangesh Sangapu <msangapu@microsoft.com>
2018-12-13 14:47:06 -0600

    Updated script so it runs locally with AZ CLI (#233)
    * Updated script so it runs locally with AZ CLI
    
    * Reverting .sh extension

--- app-service/deploy-linux-acr/deploy-linux-acr.sh
+++ app-service/deploy-linux-acr/deploy-linux-acr.sh
@@ -1,33 +1,29 @@
-#/bin/bash
+# Create a resource group.
+az group create --name myResourceGroup --location westus
 
-# Variables
-appName="AppServiceLinuxDocker$RANDOM"
-location="westeurope"
-dockerContainerUser="<replace-with-docker-user>"    
-dockerHubContainerName="<replace-with-docker-container>"
-dockerContainerVersion="latest"
+# Create an Azure Container Registry.
+az acr create --name <registry_name> --resource-group myResourceGroup --location westus --sku basic --admin-enabled true --query loginServer --output tsv
 
-# Create a Resource Group
-az group create --name myResourceGroup --location $location
+# Show ACR credentials.
+az acr credential show --name <registry_name> --resource-group myResourceGroup --query [username,passwords[?name=='password'].value] --output tsv
 
-# Create Azure Container Registry
-acrUrl=$(az acr create --name $appName --resource-group myResourceGroup --location $location --admin-enabled true --query loginServer --output tsv)
-acrCreds=($(az acr credential show --name $appName --resource-group myResourceGroup --query [username,password] --output tsv))
+# Before continuing, save the ACR credentials and registry URL. You will need this information in the commands below.
 
-# Pull from Docker
-docker login ${acrUrl} -u ${acrCreds[0]} -p ${acrCreds[1]}
-docker pull $dockerContainerUser/$dockerHubContainerName:$dockerContainerVersion
-docker tag $dockerContainerUser/$dockerHubContainerName:$dockerContainerVersion ${acrUrl}/$dockerHubContainerName:$dockerContainerVersion
-docker push ${acrUrl}/$dockerHubContainerName:$dockerContainerVersion
+# Pull from Docker.
+docker login <acr_registry_name>.azurecr.io -u <registry_user>
+docker pull <registry_user/container_name:version>
 
-# Create an App Service Plan
-az appservice plan create --name AppServiceLinuxDockerPlan --resource-group myResourceGroup --location $location --is-linux --sku S1
+# Tag Docker image.
+docker tag <registry_user/container_name:version> <acr_registry_name>.azurecr.io/<container_name:version>
 
-# Create a Web App
-az webapp create --name $appName --plan AppServiceLinuxDockerPlan --resource-group myResourceGroup
+# Push container image to Azure Container Registry.
+docker push <acr_registry_name>.azurecr.io/<container_name:version>
 
-# Configure Web App with a Custom Docker Container from Docker Hub
-az webapp config container set --docker-registry-server-url http://${acrUrl} --docker-custom-image-name ${acrUrl}/$dockerHubContainerName:$dockerContainerVersion --docker-registry-server-user ${acrCreds[0]} --docker-registry-server-password ${acrCreds[1]} --name $appName --resource-group myResourceGroup
+# Create an App Service plan.
+az appservice plan create --name AppServiceLinuxDockerPlan --resource-group myResourceGroup --location westus --is-linux --sku S1
 
-# Copy the result of the following command into a browser to see the web app.
-echo http://$appName.azurewebsites.net
+# Create a web app.
+az webapp create --name <app_name> --plan AppServiceLinuxDockerPlan --resource-group myResourceGroup --deployment-container-image-name <acr_registry_name>.azurecr.io/<container_name:version>
+
+# Configure web app with a custom Docker Container from Azure Container Registry.
+az webapp config container set --resource-group myResourceGroup --name <app_name> --docker-registry-server-url http://<acr_registry_name>.azurecr.io --docker-registry-server-user <registry_user> --docker-registry-server-password <registry_password>
