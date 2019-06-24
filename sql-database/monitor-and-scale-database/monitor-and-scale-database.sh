1d8a4e5b59c0e87e778f43cbe0d53c6a44d42fcd
CarlRabeler <carlrab@microsoft.com>
2019-06-24 15:42:05 -0700

    more password

--- sql-database/monitor-and-scale-database/monitor-and-scale-database.sh
+++ sql-database/monitor-and-scale-database/monitor-and-scale-database.sh
@@ -9,7 +9,7 @@ export location=westus2
 
 # Set an admin login and password for your database
 export adminlogin=ServerAdmin
-export password=<EnterYourComplexPasswordHere>
+export password=<EnterYourComplexPasswordHere1>
 # export password=P@ssw0rd-$RANDOM
 
 # The logical server name has to be unique in the system
