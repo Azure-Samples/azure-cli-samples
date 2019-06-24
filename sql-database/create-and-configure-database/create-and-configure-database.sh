c06d78d2b515d02111bd19c9991a3c547b1e8457
CarlRabeler <carlrab@microsoft.com>
2019-06-24 15:40:04 -0700

    more password

--- sql-database/create-and-configure-database/create-and-configure-database.sh
+++ sql-database/create-and-configure-database/create-and-configure-database.sh
@@ -10,6 +10,7 @@ export location=westus2
 # Set an admin login and password for your database
 export adminlogin=ServerAdmin
 export password=<EnterYourComplexPasswordHere>
+# export password=P@ssw0rd-$RANDOM
 
 # The logical server name has to be unique in the system
 export servername=server-$RANDOM
