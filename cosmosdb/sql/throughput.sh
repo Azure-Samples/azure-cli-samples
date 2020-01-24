798ea99b269509ffb3f68c4e8f04ea60c2e6d064
Mark Brown <mjbrown@microsoft.com>
2020-01-24 06:55:38 -0800

    add min throughput

--- cosmosdb/sql/throughput.sh
+++ cosmosdb/sql/throughput.sh
@@ -1,54 +1,100 @@
 !/bin/bash
 
-# Update throughput for a SQL API database and container
+# Throughput operations for a SQL API database and container
 
 # Generate a unique 10 character alphanumeric string to ensure unique resource names
 uniqueId=$(env LC_CTYPE=C tr -dc 'a-z0-9' < /dev/urandom | fold -w 10 | head -n 1)
-
 # Variables for SQL API resources
 resourceGroupName="Group-$uniqueId"
 location='westus2'
 accountName="cosmos-$uniqueId" #needs to be lower case
 databaseName='database1'
 containerName='container1'
+originalThroughput=400
+updateThroughput=500
 
-# Create a resource group
+# Create a resource group, Cosmos account, database and container
 az group create -n $resourceGroupName -l $location
+az cosmosdb create -n $accountName -g $resourceGroupName
+az cosmosdb sql database create -a $accountName -g $resourceGroupName -n $databaseName --throughput $originalThroughput
+az cosmosdb sql container create -a $accountName -g $resourceGroupName -d $databaseName -n $containerName -p '/myPk' --throughput $originalThroughput
 
-# Create a Cosmos account for SQL API
-az cosmosdb create \
-    -n $accountName \
-    -g $resourceGroupName
 
-# Create a SQL API database with shared throughput
-az cosmosdb sql database create \
-    -a $accountName \
+# Throughput operations for SQL API database
+#   Read the current throughput
+#   Read the minimum throughput
+#   Make sure the updated throughput is not less than the minimum
+#   Update the throughput
+read -p 'Press any key to read current provisioned throughput on database'
+
+az cosmosdb sql database throughput show \
     -g $resourceGroupName \
+    -a $accountName \
     -n $databaseName \
-    --throughput 400
+    --query resource.throughput \
+    -o tsv
 
-# Create a SQL API container, default index policy, dedicated throughput
-az cosmosdb sql container create \
-    -a $accountName \
+read -p 'Press any key to read minimum throughput on database'
+
+minimumThroughput=$(az cosmosdb sql database throughput show \
     -g $resourceGroupName \
-    -d $databaseName \
-    -n $containerName \
-    -p '/id' \
-    --throughput 400
+    -a $accountName \
+    -n $databaseName \
+    --query resource.minimumThroughput \
+    -o tsv)
 
-read -p 'Press any key to increase Database throughput to 500'
+echo $minimumThroughput
+
+# Make sure the updated throughput is not less than the minimum allowed throughput
+if [ $updateThroughput -lt $minimumThroughput ]; then
+    updateThroughput=$minimumThroughput
+fi
+
+read -p 'Press any key to update database throughput'
 
 az cosmosdb sql database throughput update \
     -a $accountName \
     -g $resourceGroupName \
     -n $databaseName \
-    --throughput 400
+    --throughput $updateThroughput
 
-read -p 'Press any key to increase Container throughput to 500'
+# Throughput operations for SQL API container
+# Read the current throughput
+# Read the minimum throughput
+# Make sure the updated throughput is not less than the minimum
+# Update the throughput
+read -p 'Press any key to read current provisioned throughput on container'
+
+az cosmosdb sql container throughput show \
+    -g $resourceGroupName \
+    -a $accountName \
+    -d $databaseName \
+    -n $containerName \
+    --query resource.throughput \
+    -o tsv
+
+read -p 'Press any key to read minimum throughput on container'
+
+minimumThroughput=$(az cosmosdb sql container throughput show \
+    -g $resourceGroupName \
+    -a $accountName \
+    -d $databaseName \
+    -n $containerName \
+    --query resource.minimumThroughput \
+    -o tsv)
+
+echo $minimumThroughput
+
+# Make sure the updated throughput is not less than the minimum allowed throughput
+if [ $updateThroughput -lt $minimumThroughput ]; then
+    updateThroughput=$minimumThroughput
+fi
+
+read -p 'Press any key to update container throughput'
 
 az cosmosdb sql container throughput update \
     -a $accountName \
     -g $resourceGroupName \
     -d $databaseName \
     -n $containerName \
-    --throughput 500
+    --throughput $updateThroughput
--- cosmosdb/table/throughput.sh
+++ cosmosdb/table/throughput.sh
@@ -1,6 +1,6 @@
 !/bin/bash
 
-# Update throughput for Table API table
+# Throughput operations for a Table API table
 
 # Generate a unique 10 character alphanumeric string to ensure unique resource names
 uniqueId=$(env LC_CTYPE=C tr -dc 'a-z0-9' < /dev/urandom | fold -w 10 | head -n 1)
@@ -10,27 +10,50 @@ resourceGroupName="Group-$uniqueId"
 location='westus2'
 accountName="cosmos-$uniqueId" #needs to be lower case
 tableName='table1'
+originalThroughput=400
+updateThroughput=500
 
-# Create a resource group
+# Create a resource group, Cosmos account and table
 az group create -n $resourceGroupName -l $location
+az cosmosdb create -n $accountName -g $resourceGroupName --capabilities EnableTable
+az cosmosdb table create -a $accountName -g $resourceGroupName -n $tableName --throughput $originalThroughput
 
-# Create a Cosmos account for Table API
-az cosmosdb create \
-    -n $accountName \
-    -g $resourceGroupName \
-    --capabilities EnableTable \
 
-# Create a Table API Table
-az cosmosdb table create \
+# Throughput operations for Table API table
+#   Read the current throughput
+#   Read the minimum throughput
+#   Make sure the updated throughput is not less than the minimum
+#   Update the throughput
+
+read -p 'Press any key to get current provisioned Table throughput'
+
+az cosmosdb table throughput show \
     -a $accountName \
     -g $resourceGroupName \
     -n $tableName \
-    --throughput 400
+    --query resource.throughput \
+    -o tsv
 
-read -p 'Press any key to increase Table throughput to 500'
+read -p 'Press any key to get minimum allowable Table throughput'
+
+minimumThroughput=$(az cosmosdb table throughput show \
+    -a $accountName \
+    -g $resourceGroupName \
+    -n $tableName \
+    --query resource.minimumThroughput \
+    -o tsv)
+
+echo $minimumThroughput
+
+# Make sure the updated throughput is not less than the minimum allowed throughput
+if [ $updateThroughput -lt $minimumThroughput ]; then
+    updateThroughput=$minimumThroughput
+fi
+
+read -p 'Press any key to update Table throughput'
 
 az cosmosdb table throughput update \
     -a $accountName \
     -g $resourceGroupName \
     -n $tableName \
-    --throughput 500
+    --throughput $updateThroughput
