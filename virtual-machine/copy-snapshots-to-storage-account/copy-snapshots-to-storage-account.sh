94c9ce2d07f0ed5de5967cd5a20b5762a41ed766
BharathNimmala-MSFT <45180002+BharathNimmala-MSFT@users.noreply.github.com>
2020-03-22 20:00:33 -0700

    (AzureCXP) Update typo for variables
    Based on customer feedback , updating the variables with $ sign so that it reflects the script accordingly. 
    
    https://github.com/MicrosoftDocs/azure-docs/issues/50668

--- virtual-machine/copy-snapshots-to-storage-account/copy-snapshots-to-storage-account.sh
+++ virtual-machine/copy-snapshots-to-storage-account/copy-snapshots-to-storage-account.sh
@@ -1,27 +1,27 @@
 #Provide the subscription Id where snapshot is created
-subscriptionId=dd80b94e-0463-4a65-8d04-c94f403879dc
+$subscriptionId=dd80b94e-0463-4a65-8d04-c94f403879dc
 
 #Provide the name of your resource group where snapshot is created
-resourceGroupName=myResourceGroupName
+$resourceGroupName=myResourceGroupName
 
 #Provide the snapshot name 
-snapshotName=mySnapshotName
+$snapshotName=mySnapshotName
 
 #Provide Shared Access Signature (SAS) expiry duration in seconds e.g. 3600.
 #Know more about SAS here: https://docs.microsoft.com/en-us/azure/storage/storage-dotnet-shared-access-signature-part-1
-sasExpiryDuration=3600
+$sasExpiryDuration=3600
 
 #Provide storage account name where you want to copy the snapshot. 
-storageAccountName=mystorageaccountname
+$storageAccountName=mystorageaccountname
 
 #Name of the storage container where the downloaded snapshot will be stored
-storageContainerName=mystoragecontainername
+$storageContainerName=mystoragecontainername
 
 #Provide the key of the storage account where you want to copy snapshot. 
-storageAccountKey=mystorageaccountkey
+$storageAccountKey=mystorageaccountkey
 
 #Provide the name of the VHD file to which snapshot will be copied.
-destinationVHDFileName=myvhdfilename
+$destinationVHDFileName=myvhdfilename
 
 az account set --subscription $subscriptionId
 
