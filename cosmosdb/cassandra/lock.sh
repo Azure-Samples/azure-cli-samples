e3f533aef15cec5652946279d5ee5c12841b4e76
Mark Brown <mjbrown@microsoft.com>
2020-06-02 14:49:27 -0700

    add resource lock samples

---
+++ cosmosdb/cassandra/lock.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+resourceGroupName='mjbArmTest'
+accountName='mjb-cassandra'
+keyspaceName='keyspace1'
+tableName='table1'
+
+lockType='CanNotDelete' # CanNotDelete or ReadOnly
+keyspaceParent="databaseAccounts/$accountName"
+tableParent="databaseAccounts/$accountName/cassandraKeyspaces/$keyspaceName"
+keyspaceLockName="$keyspaceName-Lock"
+tableLockName="$tableName-Lock"
+
+
+# Create a delete lock on keyspace
+az lock create --name $keyspaceLockName \
+    --resource-group $resourceGroupName \
+    --resource-type Microsoft.DocumentDB/cassandraKeyspaces \
+    --lock-type $lockType \
+    --parent $keyspaceParent \
+    --resource $keyspaceName
+
+# Create a delete lock on table
+az lock create --name $tableLockName \
+    --resource-group $resourceGroupName \
+    --resource-type Microsoft.DocumentDB/tables \
+    --lock-type $lockType \
+    --parent $tableParent \
+    --resource $tableName
+
+# List all locks on a Cosmos account
+az lock list --resource-group $resourceGroupName \
+    --resource-name $accountName \
+    --namespace Microsoft.DocumentDB \
+    --resource-type databaseAccounts
+
+# Delete lock on keyspace
+lockid=$(az lock show --name $keyspaceLockName \
+        --resource-group $resourceGroupName \
+        --resource-type Microsoft.DocumentDB/cassandraKeyspaces \
+        --resource $keyspaceName --parent $keyspaceParent \
+        --output tsv --query id)
+az lock delete --ids $lockid
+
+# Delete lock on table
+lockid=$(az lock show --name $tableLockName \
+        --resource-group $resourceGroupName \
+        --resource-type Microsoft.DocumentDB/tables \
+        --resource-name $tableName \
+        --parent $tableParent \
+        --output tsv --query id)
+az lock delete --ids $lockid
---
+++ cosmosdb/sql/lock.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+resourceGroupName='myResourceGroup'
+accountName='my-cosmos-account'
+databaseName='myDatabase'
+containerName='myContainer'
+
+lockType='CanNotDelete' # CanNotDelete or ReadOnly
+databaseParent="databaseAccounts/$accountName"
+containerParent="databaseAccounts/$accountName/sqlDatabases/$databaseName"
+databaseLockName="$databaseName-Lock"
+containerLockName="$containerName-Lock"
+
+# Create a delete lock on database
+az lock create --name $databaseLockName \
+    --resource-group $resourceGroupName \
+    --resource-type Microsoft.DocumentDB/sqlDatabases \
+    --lock-type $lockType \
+    --parent $databaseParent \
+    --resource $databaseName
+
+# Create a delete lock on container
+az lock create --name $containerLockName \
+    --resource-group $resourceGroupName \
+    --resource-type Microsoft.DocumentDB/containers \
+    --lock-type $lockType \
+    --parent $containerParent \
+    --resource $containerName
+
+# List all locks on a Cosmos account
+az lock list --resource-group $resourceGroupName \
+    --resource-name $accountName \
+    --namespace Microsoft.DocumentDB \
+    --resource-type databaseAccounts
+
+# Delete lock on database
+lockid=$(az lock show --name $databaseLockName \
+        --resource-group $resourceGroupName \
+        --resource-type Microsoft.DocumentDB/sqlDatabases \
+        --resource $databaseName \
+        --parent $databaseParent \
+        --output tsv --query id)
+az lock delete --ids $lockid
+
+# Delete lock on container
+lockid=$(az lock show --name $containerLockName \
+        --resource-group $resourceGroupName \
+        --resource-type Microsoft.DocumentDB/containers \
+        --resource-name $containerName \
+        --parent $containerParent \
+        --output tsv --query id)
+az lock delete --ids $lockid
