76ad8af5e99bbd022d394b516e1512e95b5ecb08
Dan Lepow <danlep@microsoft.com>
2021-07-08 12:11:33 -0700

    [ACR] Updated SP creation script (#306)

--- container-registry/service-principal-create/service-principal-create.sh
+++ container-registry/service-principal-create/service-principal-create.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This script requires Azure CLI version 2.25.0 or later. Check version with `az --version`.
 
 # Modify for your environment.
 # ACR_NAME: The name of your Azure Container Registry
@@ -15,8 +16,8 @@ ACR_REGISTRY_ID=$(az acr show --name $ACR_NAME --query id --output tsv)
 # acrpull:     pull only
 # acrpush:     push and pull
 # owner:       push, pull, and assign roles
-SP_PASSWD=$(az ad sp create-for-rbac --name http://$SERVICE_PRINCIPAL_NAME --scopes $ACR_REGISTRY_ID --role acrpull --query password --output tsv)
-SP_APP_ID=$(az ad sp show --id http://$SERVICE_PRINCIPAL_NAME --query appId --output tsv)
+SP_PASSWD=$(az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME --scopes $ACR_REGISTRY_ID --role acrpull --query password --output tsv)
+SP_APP_ID=$(az ad sp list --display-name $SERVICE_PRINCIPAL_NAME --query [].appId --output tsv)
 
 # Output the service principal's credentials; use these in your services and
 # applications to authenticate to the container registry.
