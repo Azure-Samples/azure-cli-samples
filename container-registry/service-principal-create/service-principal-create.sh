07c10684a5de96c04654c076b8c3700265047be9
Steve Lasker <stevenlasker@hotmail.com>
2021-11-01 16:11:23 -0700

    Change SP_APP_ID and PASSWORD to USER_NAME and PASSWORD
    Signed-off-by: Steve Lasker <stevenlasker@hotmail.com>

--- container-registry/service-principal-create/service-principal-create.sh
+++ container-registry/service-principal-create/service-principal-create.sh
@@ -16,10 +16,10 @@ ACR_REGISTRY_ID=$(az acr show --name $ACR_NAME --query "id" --output tsv)
 # acrpull:     pull only
 # acrpush:     push and pull
 # owner:       push, pull, and assign roles
-SP_PASSWD=$(az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME --scopes $ACR_REGISTRY_ID --role acrpull --query "password" --output tsv)
-SP_APP_ID=$(az ad sp list --display-name $SERVICE_PRINCIPAL_NAME --query "[].appId" --output tsv)
+PASSWORD=$(az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME --scopes $ACR_REGISTRY_ID --role acrpull --query "password" --output tsv)
+USER_NAME=$(az ad sp list --display-name $SERVICE_PRINCIPAL_NAME --query "[].appId" --output tsv)
 
 # Output the service principal's credentials; use these in your services and
 # applications to authenticate to the container registry.
-echo "Service principal ID: $SP_APP_ID"
-echo "Service principal password: $SP_PASSWD"
+echo "Service principal ID: $USER_NAME"
+echo "Service principal password: $PASSWORD"
