c355d1aec1edd9c3c21721c94b38c9e3f3eacf7c
Steve Lasker <stevenlasker@hotmail.com>
2021-11-01 14:25:27 -0700

    Add qoutes around --query parameters
    Signed-off-by: Steve Lasker <stevenlasker@hotmail.com>

--- container-registry/service-principal-create/service-principal-create.sh
+++ container-registry/service-principal-create/service-principal-create.sh
@@ -8,7 +8,7 @@ ACR_NAME=<container-registry-name>
 SERVICE_PRINCIPAL_NAME=acr-service-principal
 
 # Obtain the full registry ID for subsequent command args
-ACR_REGISTRY_ID=$(az acr show --name $ACR_NAME --query id --output tsv)
+ACR_REGISTRY_ID=$(az acr show --name $ACR_NAME --query "id" --output tsv)
 
 # Create the service principal with rights scoped to the registry.
 # Default permissions are for docker pull access. Modify the '--role'
@@ -16,7 +16,7 @@ ACR_REGISTRY_ID=$(az acr show --name $ACR_NAME --query id --output tsv)
 # acrpull:     pull only
 # acrpush:     push and pull
 # owner:       push, pull, and assign roles
-SP_PASSWD=$(az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME --scopes $ACR_REGISTRY_ID --role acrpull --query password --output tsv)
+SP_PASSWD=$(az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME --scopes $ACR_REGISTRY_ID --role acrpull --query "password" --output tsv)
 SP_APP_ID=$(az ad sp list --display-name $SERVICE_PRINCIPAL_NAME --query "[].appId" --output tsv)
 
 # Output the service principal's credentials; use these in your services and
