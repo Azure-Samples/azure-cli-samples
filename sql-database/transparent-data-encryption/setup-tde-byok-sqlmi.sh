7a5d5c7bad278b54a68ef98a9a4ae81d458b5a31
carlrab <94200262+carlrab@users.noreply.github.com>
2021-12-08 15:14:45 -0800

    fixed scripts

--- sql-database/transparent-data-encryption/setup-tde-byok-sqlmi.sh
+++ sql-database/transparent-data-encryption/setup-tde-byok-sqlmi.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 # Passed validation in Cloud Shell 12/01/2021
 
-let randomIdentifier=$RANDOM*$RANDOM
-
-#Run this script after create-manage-instance.sh script creates a managed instance
-instance="<msdocs-azuresql-instance>" # add instance here
-resourceGroup="<msdocs-azuresql-rg>" # add resource here
+#Run this script after create-manage-instance.sh script creates a managed instance, using the same variables from previous script
+#If running this script against a different existing instance, uncomment and add appropriate values to next 3 lines of code
+#let randomIdentifier=$RANDOM*$RANDOM
+#instance="<msdocs-azuresql-instance>" # add instance here
+#resourceGroup="<msdocs-azuresql-rg>" # add resource here
 
 location="East US"
-vault="msdocs-azuresql-vault-$randomIdentifier"
+vault="msdocssqlvault$randomIdentifier"
 key="msdocs-azuresql-key-$randomIdentifier"
 
 #echo assigning identity to service principal in the instance
 az sql mi update --name $instance --resource-group $resourceGroup --assign-identity
 
 echo "Creating $vault..."
-#az keyvault create --name $vault --resource-group $resourceGroup --enable-soft-delete true --location "$location"
-
 az keyvault create --name $vault --resource-group $resourceGroup --location "$location"
 
-#echo "Getting service principal id and setting policy on $vault..."
-
+echo "Getting service principal id and setting policy on $vault..."
 instanceId=$(az sql mi show --name $instance --resource-group $resourceGroup --query identity.principalId --output tsv)
+
 echo $instanceId
 az keyvault set-policy --name $vault --object-id $instanceId --key-permissions get unwrapKey wrapKey
 
@@ -35,7 +33,7 @@ az keyvault key create --name $key --vault-name $vault --size 2048
 echo "Setting security on $instance with $key..."
 keyId=$(az keyvault key show --name $key --vault-name $vault -o json --query key.kid | tr -d '"')
 
-az sql mi key create --kid $keyId --managed-instance $instance --resource-group $resource
+az sql mi key create --kid $keyId --managed-instance $instance --resource-group $resourceGroup
 az sql mi tde-key set --server-key-type AzureKeyVault --kid $keyId --managed-instance $instance --resource-group $resourceGroup 
 
 # echo "Deleting all resources"
