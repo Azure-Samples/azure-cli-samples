a86b9f33ee056db5f8b13a3dfefe02b8713816fc
carlrab <94200262+carlrab@users.noreply.github.com>
2022-01-26 19:42:03 -0600

    draft

--- virtual-machine-scale-sets/auto-scale-host-metrics/auto-scale-host-metrics.sh
+++ virtual-machine-scale-sets/auto-scale-host-metrics/auto-scale-host-metrics.sh
@@ -1,28 +1,60 @@
 #!/bin/bash
+# Passed validation in Cloud Shell on 1/26/2022
 
-sub=$(az account show --query id -o tsv)
-resourcegroup_name="myResourceGroup"
-scaleset_name="myScaleSet"
-location_name="eastus"
+let "randomIdentifier=$RANDOM*$RANDOM"
+subcriptionId=$(az account show --query id -o tsv)
+location="East US"
+resourceGroup="msdocs-vmss-rg-$randomIdentifier"
+tags="auto-scale-host-metrics-vmss"
+image="UbuntuLTS"
+scaleSet="msdocs-scaleSet-$randomIdentifier"
+upgradePolicyMode="automatic"
+instanceCount="2"
+login="msdocsadminuser"
 
 # Create a resource group
-az group create --name $resourcegroup_name --location $location_name
+echo "Creating $resourceGroup in $location..."
+az group create --name $resourceGroup --location "$location" --tag $tag
 
 # Create a scale set
 # Network resources such as an Azure load balancer are automatically created
 az vmss create \
-  --resource-group $resourcegroup_name \
-  --name $scaleset_name \
-  --image UbuntuLTS \
-  --upgrade-policy-mode automatic \
-  --instance-count 2 \
-  --admin-username azureuser \
+  --resource-group $resourceGroup \
+  --name $scaleSet \
+  --image $image \
+  --upgrade-policy-mode $upgradePolicyMode \
+  --instance-count $instanceCount \
+  --admin-username $login \
   --generate-ssh-keys
 
 # Define auto scale rules
 # These rules automatically scale up the number of VM instances by 3 instances when the average CPU load over a 5-minute
 # window exceeds 70%
 # The scale set then automatically scales in by 1 instance when the average CPU load over a 5-minute window drops below 30%
+
+# Define an autoscale profile
+# The following script sets the default, and minimum, capacity of *2* VM instances, and a maximum of *10*
+az monitor autoscale create --resourceGroup $resourceGroup /
+  --resource=$scaleSet /
+  --resource-type Microsoft.Compute/virtualMachineScaleSets \
+  --name autoscale \
+  --min-count 2 \
+  --max-count 10 \
+  --count 2
+
+# Create a rule to autoscale out
+# The following script increases the number of VM instances in a scale set when the average CPU load
+# is greater than 70% over a 5-minute period.
+# When the rule triggers, the number of VM instances is increased by three.
+
+az monitor autoscale rule create \
+  --resource-group myResourceGroup \
+  --autoscale-name autoscale \
+  --condition "Percentage CPU > 70 avg 5m" \
+  --scale out 3
+
+
+
 az monitor autoscale-settings create \
     --resource-group $resourcegroup_name \
     --name autoscale \
---
+++ virtual-machine-scale-sets/use-custom-vm-image/create-use-custom-image.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# Passed validation in Cloud Shell on 1/26/2022
+
+let "randomIdentifier=$RANDOM*$RANDOM"
+subcriptionId=$(az account show --query id -o tsv)
+location="East US"
+resourceGroup="msdocs-vmss-rg-$randomIdentifier"
+tags="create-custom-image-vmss"
+image="UbuntuLTS"
+virtualMachine="msdocs-vm-$randomIdentifier"
+login="azureuser"
+imageGallery="msdocsimagegalleryvmss$randomIdentifier"
+imageDefinition="msdocs-image-definition-vmss-$randomIdentifier"
+imageVersion="msdocs-image-version-$randomIdentifier"
+scaleSet="msdocs-scaleSet-$randomIdentifier"
+upgradePolicyMode="automatic"
+
+# Create a resource group
+echo "Creating $resourceGroup in $location..."
+az group create --name $resourceGroup --location "$location" --tag $tag
+
+# Create virtual machine from UbuntuLTS image
+echo "Create $virtualMachine from $image image"
+az vm create --resource-group $resourceGroup \
+  --name $virtualMachine \
+  --image $image \
+  --admin-username $login \
+  --generate-ssh-keys \
+  --public-ip-sku Standard
+
+# Create a resource group for images
+# echo "Creating $resourceGroup in $location..."
+# az group create --name $resourceGroup --location "$location" --tag $tag
+# az group create --name myGalleryRG --location eastus
+
+# Create an image gallery
+echo "Creating $imageGallery"
+az sig create --resource-group $resourceGroup \
+--gallery-name $imageGallery
+
+# Create image definition
+echo "Creating $imageDefinition from $imageGallery"
+az sig image-definition create --resource-group $resourceGroup \
+--gallery-name $imageGallery \
+--gallery-image-definition $imageDefinition \
+--publisher myPublisher \
+--offer myOffer \
+--sku mySKU \
+--os-type Linux \
+--os-state specialized
+# az sig image-definition create --resource-group myGalleryRG --gallery-name myGallery --gallery-image-definition myImageDefinition --publisher myPublisher --offer myOffer --sku mySKU --os-type Linux --os-state specialized
+
+# Create image version
+echo "Creating $imageVersion from $imageDefinition"
+az sig image-version create \
+   --resource-group $resourceGroup \
+   --gallery-name $imageGallery \
+   --gallery-image-definition $imageDefinition \
+   --gallery-image-version 1.0.0 \
+   --target-regions "southcentralus=1" "eastus=1" \
+   --managed-image "/subscriptions/$subcriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Compute/virtualMachines/$virtualMachine"
+
+# Create a scale set from custom image
+az vmss create \
+  --resource-group $resourceGroup \
+  --name $scaleSet \
+  --image "/subscriptions/$subcriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Compute/galleries/$imageGallery/images/$imageDefinition" \
+  --specialized
+
+# echo "Deleting all resources"
+# az group delete --name $resourceGroup -y
+
