6f58849d053387b65c3cb1ca71558c8e9dd8ad28
carlrab <94200262+carlrab@users.noreply.github.com>
2022-02-02 15:09:11 -0800

    in progress

--- virtual-network/virtual-network-cli-sample-ipv6-dual-stack-standard-load-balancer/virtual-network-cli-sample-ipv6-dual-stack-standard-load-balancer.sh
+++ virtual-network/virtual-network-cli-sample-ipv6-dual-stack-standard-load-balancer/virtual-network-cli-sample-ipv6-dual-stack-standard-load-balancer.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Passed validation in Cloud Shell 02/01/2022
 
-let "randomIdentifier=$RANDOM*$RANDOM"
-location="East US"
-resourceGroup="msdocs-azuresql-rg-$randomIdentifier"
-tag="virtual-network-cli-sample-ipv6-dual-stack"
-
-# Create a resource group
-echo "Creating $resourceGroup in $location..."
-az group create --name $resourceGroup --location "$location" --tag $tag
-
+# IMPORTANT
+# To use the IPv6 for Azure virtual network feature, you must configure your subscription only once as follows:
+# az feature register --name AllowIPv6VirtualNetwork --namespace Microsoft.Network
+# az feature register --name AllowIPv6CAOnStandardLB --namespace Microsoft.Network
+# It takes up to 30 minutes for feature registration to complete. You can check your registration status by running the following Azure CLI command:
+# az feature show --name AllowIPv6VirtualNetwork --namespace Microsoft.Network
+# az feature show --name AllowIPv6CAOnStandardLB --namespace Microsoft.Network
+# After the registration is complete, run the following command:
+# az provider register --namespace Microsoft.Network
 
 # Create a resource group
 az group create \
@@ -21,8 +21,8 @@ az network public-ip create \
 --name dsPublicIP_v4  \
 --resource-group DsResourceGroup01  \
 --location eastus  \
---sku BASIC  \
---allocation-method dynamic  \
+--sku STANDARD  \
+--allocation-method static  \
 --version IPv4
 
 # Create an IPV6 IP address
@@ -30,8 +30,8 @@ az network public-ip create \
 --name dsPublicIP_v6  \
 --resource-group DsResourceGroup01  \
 --location eastus \
---sku BASIC  \
---allocation-method dynamic  \
+--sku STANDARD  \
+--allocation-method static  \
 --version IPv6
 
 # Create public IP addresses for remote access to VMs
@@ -39,22 +39,24 @@ az network public-ip create \
 --name dsVM0_remote_access  \
 --resource-group DsResourceGroup01 \
 --location eastus  \
---sku BASIC  \
---allocation-method dynamic  \
+--sku Standard  \
+--allocation-method static  \
 --version IPv4
 
-# Create load balancer
 az network public-ip create \
 --name dsVM1_remote_access  \
 --resource-group DsResourceGroup01  \
 --location eastus  \
---sku BASIC  \
---allocation-method dynamic  \
+--sku Standard  \
+--allocation-method static  \
 --version IPv4
+
+# Create load balancer
+
 az network lb create \
 --name dsLB  \
 --resource-group DsResourceGroup01 \
---sku Basic \
+--sku Standard \
 --location eastus \
 --frontend-ip-name dsLbFrontEnd_v4  \
 --public-ip-address dsPublicIP_v4  \
@@ -86,7 +88,7 @@ az network lb rule create \
 --backend-pool-name dsLbBackEndPool_v4
 
 
-az network lb rule create 
+az network lb rule create \
 --lb-name dsLB  \
 --name dsLBrule_v6  \
 --resource-group DsResourceGroup01 \
@@ -102,7 +104,7 @@ az vm availability-set create \
 --resource-group DsResourceGroup01  \
 --location eastus \
 --platform-fault-domain-count 2  \
---platform-update-domain-count 2
+--platform-update-domain-count 2  
 
 # Create network security group
 
@@ -122,9 +124,24 @@ az network nsg rule create \
 --protocol "*"  \
 --direction Inbound  \
 --source-address-prefixes "*"  \
---source-port-ranges 3389  \
+--source-port-ranges "*"  \
 --destination-address-prefixes "*"  \
 --destination-port-ranges 3389
+
+# Create inbound rule for port 80
+az network nsg rule create \
+--name allowHTTPIn  \
+--nsg-name dsNSG1  \
+--resource-group DsResourceGroup01  \
+--priority 200  \
+--description "Allow HTTP In"  \
+--access Allow  \
+--protocol "*"  \
+--direction Inbound  \
+--source-address-prefixes "*"  \
+--source-port-ranges 80  \
+--destination-address-prefixes "*"  \
+--destination-port-ranges 80
 
 # Create outbound rule
 
@@ -132,7 +149,7 @@ az network nsg rule create \
 --name allowAllOut  \
 --nsg-name dsNSG1  \
 --resource-group DsResourceGroup01  \
---priority 100  \
+--priority 300  \
 --description "Allow All Out"  \
 --access Allow  \
 --protocol "*"  \
@@ -155,8 +172,7 @@ az network vnet subnet create \
 --name dsSubNET \
 --resource-group DsResourceGroup01 \
 --vnet-name dsVNET \
---address-prefix 10.0.0.0/24 \
---address-prefix "fd00:db8:deca:deed::/64" \
+--address-prefixes "10.0.0.0/24" "fd00:db8:deca:deed::/64" \
 --network-security-group dsNSG1
 
 # Create NICs
@@ -201,17 +217,20 @@ az network nic ip-config create \
 --vnet-name dsVNET \
 --subnet dsSubNet \
 --private-ip-address-version IPv6 \
---lb-address-pools dsLbBackEndPool_v6 
+--lb-address-pools dsLbBackEndPool_v6 \
 --lb-name dsLB
 
-# Create virtual machines
+# Create virtual machine dsVM0
+
  az vm create \
 --name dsVM0 \
 --resource-group DsResourceGroup01 \
 --nics dsNIC0 \
 --size Standard_A2 \
 --availability-set dsAVset \
---image MicrosoftWindowsServer:WindowsServer:2016-Datacenter:latest  
+--image MicrosoftWindowsServer:WindowsServer:2019-Datacenter:latest  
+
+# Create virtual machine dsVM1
 
 az vm create \
 --name dsVM1 \
@@ -219,4 +238,4 @@ az vm create \
 --nics dsNIC1 \
 --size Standard_A2 \
 --availability-set dsAVset \
---image MicrosoftWindowsServer:WindowsServer:2016-Datacenter:latest
+--image MicrosoftWindowsServer:WindowsServer:2019-Datacenter:latest 
--- virtual-network/virtual-network-cli-sample-ipv6-dual-stack/virtual-network-cli-sample-ipv6-dual-stack.sh
+++ virtual-network/virtual-network-cli-sample-ipv6-dual-stack/virtual-network-cli-sample-ipv6-dual-stack.sh
@@ -1,50 +1,52 @@
+#!/bin/bash
+# Passed validation in Cloud Shell 02/01/2022
+
+# IMPORTANT
+# To use the IPv6 for Azure virtual network feature, you must configure your subscription only once as follows:
+# az feature register --name AllowIPv6VirtualNetwork --namespace Microsoft.Network
+# az feature register --name AllowIPv6CAOnStandardLB --namespace Microsoft.Network
+# It takes up to 30 minutes for feature registration to complete. You can check your registration status by running the following Azure CLI command:
+# az feature show --name AllowIPv6VirtualNetwork --namespace Microsoft.Network
+# az feature show --name AllowIPv6CAOnStandardLB --namespace Microsoft.Network
+# After the registration is complete, run the following command:
+# az provider register --namespace Microsoft.Network
+
+let "randomIdentifier=$RANDOM*$RANDOM"
+location="East US"
+resourceGroup="msdocs-azuresql-rg-$randomIdentifier"
+tag="virtual-network-cli-sample-ipv6-dual-stack-$randomIdentifier"
+ipV4PublicIp="msdocs-ipV4-public-ip-address-$randomIdentifier"
+ipV6PublicIp="msdocs-ipV6-public-ip-address-$randomIdentifier"
+ipV4RemoteAccessVm0="msdocs-ipV4-pubic-ip-for-vm0-remote-access-$randomIdentifier"
+ipV4RemoteAccessVm1="msdocs-ipV4-pubic-ip-for-vm1-remote-access-$randomIdentifier"
+sku="BASIC"
+loadBalancer="msdocs-load-balances-$randomIdentifier"
+
+
 # Create a resource group
-az group create \
---name DsResourceGroup01 \
---location eastus
+echo "Creating $resourceGroup in $location..."
+az group create --name $resourceGroup --location "$location" --tag $tag
 
 # Create an IPV4 IP address
-az network public-ip create \
---name dsPublicIP_v4  \
---resource-group DsResourceGroup01  \
---location eastus  \
---sku STANDARD  \
---allocation-method static  \
---version IPv4
+echo "Creating $ipV4PublicIp"
+az network public-ip create --name $ipV4PublicIp  --resource-group $resourceGroup  --location "$location"  --sku $sku  --allocation-method dynamic  --version IPv4
 
 # Create an IPV6 IP address
-az network public-ip create \
---name dsPublicIP_v6  \
---resource-group DsResourceGroup01  \
---location eastus \
---sku STANDARD  \
---allocation-method static  \
---version IPv6
+echo "Creating $ipV6PublicIp"
+az network public-ip create --name $ipV6PublicIp  --resource-group $resourceGroup  --location "$location" --sku $sku  --allocation-method dynamic  --version IPv6
 
 # Create public IP addresses for remote access to VMs
-az network public-ip create \
---name dsVM0_remote_access  \
---resource-group DsResourceGroup01 \
---location eastus  \
---sku Standard  \
---allocation-method static  \
---version IPv4
-
-az network public-ip create \
---name dsVM1_remote_access  \
---resource-group DsResourceGroup01  \
---location eastus  \
---sku Standard  \
---allocation-method static  \
---version IPv4
+echo "Creating $ipV4RemoteAccessVm0 and $ipV4RemoteAccessVm1"
+az network public-ip create --name $ipV4RemoteAccessVm0 --resource-group $resourceGroup--loca tion "$location" --sku $sku --allocation-method dynamic --version IPv4
+az network public-ip create --name $ipV4RemoteAccessVm1 --resource-group $resourceGroup --location "$location" --sku $sku --allocation-method dynamic --version IPv4
 
 # Create load balancer
-
+echo "Creating $loadBalancer"
 az network lb create \
---name dsLB  \
---resource-group DsResourceGroup01 \
---sku Standard \
---location eastus \
+--name $loadBalancer  \
+--resource-group $resourceGroup \
+--sku $sku \
+--location "$location" \
 --frontend-ip-name dsLbFrontEnd_v4  \
 --public-ip-address dsPublicIP_v4  \
 --backend-pool-name dsLbBackEndPool_v4
@@ -75,7 +77,7 @@ az network lb rule create \
 --backend-pool-name dsLbBackEndPool_v4
 
 
-az network lb rule create \
+az network lb rule create 
 --lb-name dsLB  \
 --name dsLBrule_v6  \
 --resource-group DsResourceGroup01 \
@@ -91,7 +93,7 @@ az vm availability-set create \
 --resource-group DsResourceGroup01  \
 --location eastus \
 --platform-fault-domain-count 2  \
---platform-update-domain-count 2  
+--platform-update-domain-count 2
 
 # Create network security group
 
@@ -111,24 +113,9 @@ az network nsg rule create \
 --protocol "*"  \
 --direction Inbound  \
 --source-address-prefixes "*"  \
---source-port-ranges "*"  \
+--source-port-ranges 3389  \
 --destination-address-prefixes "*"  \
 --destination-port-ranges 3389
-
-# Create inbound rule for port 80
-az network nsg rule create \
---name allowHTTPIn  \
---nsg-name dsNSG1  \
---resource-group DsResourceGroup01  \
---priority 200  \
---description "Allow HTTP In"  \
---access Allow  \
---protocol "*"  \
---direction Inbound  \
---source-address-prefixes "*"  \
---source-port-ranges 80  \
---destination-address-prefixes "*"  \
---destination-port-ranges 80
 
 # Create outbound rule
 
@@ -136,7 +123,7 @@ az network nsg rule create \
 --name allowAllOut  \
 --nsg-name dsNSG1  \
 --resource-group DsResourceGroup01  \
---priority 300  \
+--priority 100  \
 --description "Allow All Out"  \
 --access Allow  \
 --protocol "*"  \
@@ -159,7 +146,8 @@ az network vnet subnet create \
 --name dsSubNET \
 --resource-group DsResourceGroup01 \
 --vnet-name dsVNET \
---address-prefixes "10.0.0.0/24" "fd00:db8:deca:deed::/64" \
+--address-prefix 10.0.0.0/24 \
+--address-prefix "fd00:db8:deca:deed::/64" \
 --network-security-group dsNSG1
 
 # Create NICs
@@ -204,20 +192,17 @@ az network nic ip-config create \
 --vnet-name dsVNET \
 --subnet dsSubNet \
 --private-ip-address-version IPv6 \
---lb-address-pools dsLbBackEndPool_v6 \
+--lb-address-pools dsLbBackEndPool_v6 
 --lb-name dsLB
 
-# Create virtual machine dsVM0
-
+# Create virtual machines
  az vm create \
 --name dsVM0 \
 --resource-group DsResourceGroup01 \
 --nics dsNIC0 \
 --size Standard_A2 \
 --availability-set dsAVset \
---image MicrosoftWindowsServer:WindowsServer:2019-Datacenter:latest  
-
-# Create virtual machine dsVM1
+--image MicrosoftWindowsServer:WindowsServer:2016-Datacenter:latest  
 
 az vm create \
 --name dsVM1 \
@@ -225,4 +210,4 @@ az vm create \
 --nics dsNIC1 \
 --size Standard_A2 \
 --availability-set dsAVset \
---image MicrosoftWindowsServer:WindowsServer:2019-Datacenter:latest 
+--image MicrosoftWindowsServer:WindowsServer:2016-Datacenter:latest
