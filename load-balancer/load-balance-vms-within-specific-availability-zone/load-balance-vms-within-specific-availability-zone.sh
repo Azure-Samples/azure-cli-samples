8fd34a3db9cb09d1b56fe3fb89acd5e659d980f8
carlrab <94200262+carlrab@users.noreply.github.com>
2022-03-04 11:50:08 -0600

    remove continuation characters

--- load-balancer/load-balance-vms-within-specific-availability-zone/load-balance-vms-within-specific-availability-zone.sh
+++ load-balancer/load-balance-vms-within-specific-availability-zone/load-balance-vms-within-specific-availability-zone.sh
@@ -33,63 +33,33 @@ az group create --name $resourceGroup --location "$location" --tag $tag
 
 # Create a virtual network and a subnet.
 echo "Creating $vNet and $subnet"
-az network vnet create \
---resource-group $resourceGroup \
---name $vNet \
---location "$location" \
---subnet-name $subnet
+az network vnet create --resource-group $resourceGroup --name $vNet --location "$location" --subnet-name $subnet
 
 # Create a zonal Standard public IP address for load balancer.
 echo "Creating $loadBalancerPublicIp"
-az network public-ip create \
---resource-group $resourceGroup \
---name $loadBalancerPublicIp \
---sku $ipSku \
---zone $zone
+az network public-ip create --resource-group $resourceGroup --name $loadBalancerPublicIp --sku $ipSku --zone $zone
 
 # Create an Azure Load Balancer.
 echo "Creating $loadBalancer with $frontEndIP and $backEndPool"
-az network lb create \
---resource-group $resourceGroup \
---name $loadBalancer \
---public-ip-address $loadBalancerPublicIp \
---frontend-ip-name $frontEndIp \
---backend-pool-name $backEndPool \
---sku $ipSku
+az network lb create --resource-group $resourceGroup --name $loadBalancer --public-ip-address $loadBalancerPublicIp --frontend-ip-name $frontEndIp --backend-pool-name $backEndPool --sku $ipSku
 
 # Create an LB probe on port 80.
 echo "Creating $probe80 in $loadBalancer"
-az network lb probe create \
---resource-group $resourceGroup \
---lb-name $loadBalancer \
---name $probe80 \
---protocol tcp \
---port 80
+az network lb probe create --resource-group $resourceGroup --lb-name $loadBalancer --name $probe80 --protocol tcp --port 80
 
 # Create an LB rule for port 80.
 echo "Creating $loadBalancerRuleWeb for $loadBalancer"
-az network lb rule create \
---resource-group $resourceGroup \
---lb-name $loadBalancer \
---name $loadBalancerRuleWeb \
---protocol tcp \
---frontend-port 80 \
---backend-port 80 \
---frontend-ip-name $frontEndIp \
---backend-pool-name $backEndPool \
---probe-name $probe80
+az network lb rule create --resource-group $resourceGroup --lb-name $loadBalancer --name $loadBalancerRuleWeb --protocol tcp --frontend-port 80 --backend-port 80 --frontend-ip-name $frontEndIp --backend-pool-name $backEndPool --probe-name $probe80
 
 # Create three NAT rules for port 22.
 echo "Creating three NAT rules named $loadBalancerRuleSSH"
 for i in `seq 1 3`; do
-  az network lb inbound-nat-rule create --resource-group $resourceGroup --lb-name $loadBalancer --name $loadBalancerRuleSSH$i --protocol tcp --frontend-port 422$i --backend-port 22 --frontend-ip-name $frontEndIp
+az network lb inbound-nat-rule create --resource-group $resourceGroup --lb-name $loadBalancer --name $loadBalancerRuleSSH$i --protocol tcp --frontend-port 422$i --backend-port 22 --frontend-ip-name $frontEndIp
 done
 
 # Create a network security group
 echo "Creating $networkSecurityGroup"
-az network nsg create \
---resource-group $resourceGroup \
---name $networkSecurityGroup
+az network nsg create --resource-group $resourceGroup --name $networkSecurityGroup
 
 # Create a network security group rule for port 22.
 echo "Creating $networkSecurityGroupRuleSSH in $networkSecurityGroup for port 22"
@@ -102,20 +72,13 @@ az network nsg rule create --resource-group $resourceGroup --nsg-name $networkSe
 # Create three virtual network cards and associate with public IP address and NSG.
 echo "Creating three NICs named $nic for $vNet and $subnet"
 for i in `seq 1 3`; do
-  az network nic create --resource-group $resourceGroup --name $nic$i --vnet-name $vNet --subnet $subnet --network-security-group $networkSecurityGroup --lb-name $loadBalancer --lb-address-pools $backEndPool --lb-inbound-nat-rules $loadBalancerRuleSSH$i
+az network nic create --resource-group $resourceGroup --name $nic$i --vnet-name $vNet --subnet $subnet --network-security-group $networkSecurityGroup --lb-name $loadBalancer --lb-address-pools $backEndPool --lb-inbound-nat-rules $loadBalancerRuleSSH$i
 done
 
 # Create three virtual machines, this creates SSH keys if not present.
 echo "Creating three VMs named $vm with $nic using $image"
 for i in `seq 1 3`; do
-  az vm create --resource-group $resourceGroup \
-  --name $vm$i \
-  --zone $zone \
-  --nics $nic$i \
-  --image $image \
-  --admin-username $login \
-  --generate-ssh-keys \
-  --no-wait
+az vm create --resource-group $resourceGroup --name $vm$i --zone $zone --nics $nic$i --image $image --admin-username $login --generate-ssh-keys --no-wait
 done
 
 # List the virtual machines
