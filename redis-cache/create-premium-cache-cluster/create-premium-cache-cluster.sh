d29c4eb46f4ef2efdd0ed0a0efc67a5a7f5f30c2
carlrab <94200262+carlrab@users.noreply.github.com>
2022-03-11 15:21:33 -0600

    create 2 files from 5

--- redis-cache/create-premium-cache-cluster/create-premium-cache-cluster.sh
+++ redis-cache/create-premium-cache-cluster/create-premium-cache-cluster.sh
@@ -7,4 +7,3 @@ az group create --name contosoGroup --location eastus
 
 # Create a Premium P1 (6 GB) Redis Cache with clustering enabled and 2 shards (for a total of 12 GB)
 az redis create --name contosoCache --resource-group contosoGroup --location eastus --vm-size P1 --sku Premium --shard-count 2
-
