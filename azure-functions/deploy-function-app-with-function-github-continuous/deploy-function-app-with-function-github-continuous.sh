63baa87da792d9aa4a5d2cc2f1946b22cb89cdf4
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2022-03-27 19:40:44 +0000

    add --tags option to az group create

--- azure-functions/deploy-function-app-with-function-github-continuous/deploy-function-app-with-function-github-continuous.sh
+++ azure-functions/deploy-function-app-with-function-github-continuous/deploy-function-app-with-function-github-continuous.sh
@@ -21,7 +21,7 @@ gitrepo=https://github.com/Azure-Samples/functions-quickstart-javascript
 
 # Create a resource group.
 echo "Creating $resourceGroup in ""$location""..."
-az group create --name $resourceGroup --location "$location"
+az group create --name $resourceGroup --location "$location" --tags $tag
 
 # Create an Azure storage account in the resource group.
 echo "Creating $storage"
