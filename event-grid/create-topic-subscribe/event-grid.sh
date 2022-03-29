14797375ce3c1f03eeb38ed33a879f2a6cd6a54f
carlrab <94200262+carlrab@users.noreply.github.com>
2022-03-29 08:56:06 -0500

    first two scripts combined and working

--- event-grid/create-topic-subscribe/event-grid.sh
+++ event-grid/create-topic-subscribe/event-grid.sh
@@ -11,12 +11,18 @@ subscriptionId="$(az account show --query id -o tsv)"
 resourceGroup="msdocs-event-grid-rg-$randomIdentifier"
 tags="event-grid"
 topic="msdocs-event-grid-topic-$randomIdentifier"
+site="msdocs-event-grid-site-$randomIdentifier"
 eventSubscription="msdocs-event-subscription-$randomIdentifier"
-storageName="msdocsstorage$randomIdentifier"
+webappEndpoint="https://$site.azurewebsites.net/api/updates"
+storage="msdocsstorage$randomIdentifier"
 
 # Create a resource group
 echo "Creating  in "$location"..."
 az group create --name $resourceGroup --location "$location" --tag $tag
+
+# Enable and then show the Event Grid resource provider
+az provider register --namespace Microsoft.EventGrid
+az provider show --namespace Microsoft.EventGrid --query "registrationState"
 
 # Create custom topic
 echo "Creating $topic"
@@ -25,21 +31,33 @@ az eventgrid topic create \
 --name $topic \
 --location "$location"
 
-# Retrieve endpoint and key to use when publishing to the topic
-endpoint=$(az eventgrid topic show --name $topic -g $resourceGroup --query "endpoint" --output tsv)
+# Create a message endpoint
+echo "Creating $site"
+az deployment group create \
+  --resource-group gridResourceGroup \
+  --template-uri "https://raw.githubusercontent.com/Azure-Samples/azure-event-grid-viewer/master/azuredeploy.json" \
+  --parameters siteName=$site hostingPlanName=viewerhost
+
+# To view your web app, navigate to https://<your-site-name>.azurewebsites.net
+
+# Subscribe to custom topic
+az eventgrid event-subscription create \
+  --source-resource-id "/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.EventGrid/topics/$topic" \
+  --name demoViewerSub \
+  --endpoint $webappEndpoint
+
+# View your web app again to see the subscription validation event.
+# Select the eye icon to expand the event data
+  
+# Send an event to your custom topic
+url=$(az eventgrid topic show --name $topic -g $resourceGroup --query "endpoint" --output tsv)
 key=$(az eventgrid topic key list --name $topic -g $resourceGroup --query "key1" --output tsv)
-echo $endpoint
+echo $url
 echo $key
+event='[ {"id": "'"$RANDOM"'", "eventType": "recordInserted", "subject": "myapp/vehicles/motorcycles", "eventTime": "'`date +%Y-%m-%dT%H:%M:%S%z`'", "data":{ "make": "Ducati", "model": "Monster"},"dataVersion": "1.0"} ]'
+curl -X POST -H "aeg-sas-key: $key" -d "$event" $url
 
-# Subscribe to the Azure subscription.
-echo "Creating $eventSubscription"
-az eventgrid event-subscription create \
---name $eventSubscription --endpoint $endpoint
-
-az eventgrid event-subscription create \
---name $eventSubscription \
---endpoint $endpoint \
---source-resource-id /subscriptions/$subscriptionId
+# View your web app again to see the event that you just sent
 
 # Create the Blob storage account. 
 az storage account create \
@@ -57,7 +75,7 @@ echo $storageId
 az eventgrid event-subscription create \
   --source-resource-id $storageId \
   --name demoSubToStorage \
-  --endpoint $endpoint
+  --endpoint $url
 
 
 # </FullScript>
