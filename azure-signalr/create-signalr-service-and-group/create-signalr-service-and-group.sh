b55180aee66a2773c429e7ff53e4678874d6ec82
carlrab <94200262+carlrab@users.noreply.github.com>
2022-03-30 13:46:24 -0500

    upate and standardize scripts

--- azure-signalr/create-signalr-service-and-group/create-signalr-service-and-group.sh
+++ azure-signalr/create-signalr-service-and-group/create-signalr-service-and-group.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
+# Passed validation in Cloud Shell on 3/30/2022
 
-# Generate a unique suffix for the service name
-let randomNum=$RANDOM*$RANDOM
+# <FullScript>
+# Create a SignalR Service
 
-# Generate a unique service and group name with the suffix
-SignalRName=SignalRTestSvc$randomNum
-#resource name must be lowercase
-mySignalRSvcName=${SignalRName,,}
-myResourceGroupName=$SignalRName"Group"
+# Variable block
+let "randomIdentifier=$RANDOM*$RANDOM"
+location="East US"
+resourceGroup="msdocs-azure-signalr-rg-$randomIdentifier"
+tag="create-signal-service-and-group"
+signalRSvc=msdocs-signalr-svc-$randomIdentifier
+signalRSku="Standard_S1"
+unitCount="1"
+serviceMode="Default"
 
-# Create resource group 
-az group create --name $myResourceGroupName --location eastus
+# Create a resource group
+echo "Creating $resourceGroup in "$location"..."
+az group create --name $resourceGroup --location "$location" --tag $tag
 
 # Create the Azure SignalR Service resource
+echo "Creating $signalRSvc"
 az signalr create \
-  --name $mySignalRSvcName \
-  --resource-group $myResourceGroupName \
-  --sku Standard_S1 \
-  --unit-count 1 \
-  --service-mode Default
+  --name $signalRSvc \
+  --resource-group $resourceGroup \
+  --sku $signalRSku \
+  --unit-count $unitCount \
+  --service-mode $serviceMode
 
 # Get the SignalR primary connection string 
-primaryConnectionString=$(az signalr key list --name $mySignalRSvcName \
-  --resource-group $myResourceGroupName --query primaryConnectionString -o tsv)
+primaryConnectionString=$(az signalr key list --name $signalRSvc \
+  --resource-group $resourceGroup --query primaryConnectionString -o tsv)
 
 echo "$primaryConnectionString"
+# </FullScript>
+
+# echo "Deleting all resources"
+# az group delete --name $resourceGroup -y
