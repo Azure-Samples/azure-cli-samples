c3326f8003176fe684ca859077045c230d316727
Daniel Larsen <dalars@microsoft.com>
2022-04-14 08:58:09 +1200

    #412 - fix custom domain name sample, script 1/2 (WIP)

--- azure-front-door/deploy-custom-domain/deploy-custom-domain.sh
+++ azure-front-door/deploy-custom-domain/deploy-custom-domain.sh
@@ -6,32 +6,32 @@ set -e
 # See ./README.md for notes
 # Please use latest version of AZ CLI
 
+# <FullScript>
+# Deploy a Custom Domain name and TLS certificate on an Azure Front Door front-end.
 
 # VARIABLES
 # Change these hardcoded values if required
 
+# Pass custom domain name parameter as environment variable named CUSTOM_DOMAIN_NAME
+
 # Hashing Username for UID instead of using $RANDOM so that this script is idempotent
-uid=$(echo -n $USER | openssl dgst -sha256)
-uid=${uid:9:8}
+let "randomIdentifier=$RANDOM*$RANDOM"
 location='AustraliaEast'
-loc='aue'
-rg="frontdoor-$uid-rg"
-echo $rg
-tags=('sample=deploy-custom-domain' 'repo=Azure-Samples/azure-cli-samples')
+resourceGroup="msdocs-frontdoor-rg-$randomIdentifier"
+tag='deploy-custom-domain'
 
-domainName='www.contoso.com'
-storage="fd$uid$loc"
+storage="msdocsafd$randomIdentifier"
 
-frontDoor="frontdoor-$uid"
+frontDoor="msdocs-frontdoor-$randomIdentifier"
 frontDoorFrontEnd='www-contoso'
 
 
 # RESOURCE GROUP
-az group create -n $rg -l $location --tags $tags
+az group create -n $resourceGroup -l $location --tags $tag
 
 
 # STORAGE ACCOUNT
-az storage account create -n $storage -g $rg -l $location --sku Standard_LRS --kind StorageV2
+az storage account create -n $storage -g $resourceGroup -l $location --sku Standard_LRS --kind StorageV2
 
 # Make Storage Account a SPA
 az storage blob service-properties update --account-name $storage --static-website \
@@ -49,27 +49,32 @@ echo $spaUrl
 
 
 # FRONT DOOR
-az network front-door create -n $frontDoor -g $rg --tags $tags --accepted-protocols Http Https --backend-address $spaUrl
+az network front-door create -n $frontDoor -g $resourceGroup --tags $tag --accepted-protocols Http Https --backend-address $spaUrl
 
-# Create a frontend for the custom domain
-az network front-door frontend-endpoint create --front-door-name $frontDoor --host-name $domainName \
-    --name $frontDoorFrontEnd -g $rg --session-affinity-enabled 'Disabled'
+if [ "$CUSTOM_DOMAIN_NAME" != '' ]; 
+   then 
+        # Create a frontend for the custom domain
+        az network front-door frontend-endpoint create --front-door-name $frontDoor --host-name $CUSTOM_DOMAIN_NAME \
+            --name $frontDoorFrontEnd -g $resourceGroup --session-affinity-enabled 'Disabled'
 
-# Update the default routing rule to include the new frontend
-az network front-door routing-rule update --front-door-name $frontDoor -n 'DefaultRoutingRule' -g $rg \
-    --caching 'Enabled' --accepted-protocols 'HttpsOnly' \
-    --frontend-endpoints 'DefaultFrontendEndpoint' $frontDoorFrontEnd
+        # Update the default routing rule to include the new frontend
+        az network front-door routing-rule update --front-door-name $frontDoor -n 'DefaultRoutingRule' -g $resourceGroup \
+            --caching 'Enabled' --accepted-protocols 'HttpsOnly' \
+            --frontend-endpoints 'DefaultFrontendEndpoint' $frontDoorFrontEnd
 
-# Create http redirect to https routing rule
-az network front-door routing-rule create -f $frontDoor -g $rg -n 'httpRedirect' \
-    --frontend-endpoints $frontDoorFrontEnd --accepted-protocols 'Http' --route-type 'Redirect' \
-    --patterns '/*' --redirect-protocol 'HttpsOnly'
+        # Create http redirect to https routing rule
+        az network front-door routing-rule create -f $frontDoor -g $resourceGroup -n 'httpRedirect' \
+            --frontend-endpoints $frontDoorFrontEnd --accepted-protocols 'Http' --route-type 'Redirect' \
+            --patterns '/*' --redirect-protocol 'HttpsOnly'
 
-# Enable HTTPS. This command will return quickly but provisioning can take up to an hour to complete
-az network front-door frontend-endpoint enable-https \
-    --front-door-name $frontDoor -n $frontDoorFrontEnd -g $rg 
+        # Enable HTTPS. This command will return quickly but provisioning can take up to an hour to complete
+        az network front-door frontend-endpoint enable-https \
+            --front-door-name $frontDoor -n $frontDoorFrontEnd -g $resourceGroup
+    else
+        echo "Custom domain name frontend not created because environment variable CUSTOM_DOMAIN_NAME not provided"
+fi
 
+# </FullScript>
 
-echo "https://$frontDoor.azurefd.net"
-echo "http://$domainName"
-echo "https://$domainName"
+# echo "Deleting all resources"
+# az group delete --name $resourceGroup -y
