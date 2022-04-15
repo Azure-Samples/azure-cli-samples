d552a77a89fcb80d10bef45a016c1a244bd76cf2
carlrab <94200262+carlrab@users.noreply.github.com>
2022-04-15 12:20:44 -0500

    first script

--- app-service/deploy-ftp/deploy-ftp.sh
+++ app-service/deploy-ftp/deploy-ftp.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
+# Passed validation in Cloud Shell on 1/13/2022
 
-warurl=https://raw.githubusercontent.com/Azure-Samples/html-docs-hello-world/master/index.html
-webappname=mywebapp$RANDOM
+# <FullScript>
+# set -e # exit if error
+# Create an App Service app and deploy files with FTP
+# Variable block
+let "randomIdentifier=$RANDOM*$RANDOM"
+location="East US"
+resourceGroup="msdocs-app-service-rg-$randomIdentifier"
+tag="deploy-ftp.sh"
+warurl="https://raw.githubusercontent.com/Azure-Samples/html-docs-hello-world/master/index.html"
+appServicePlan="msdocs-app-service-plan-$randomIdentifier"
+webapp="msdocs-web-app-$randomIdentifier"
 
 # Download sample static HTML page
 curl $warurl --output index.html
 
 # Create a resource group.
-az group create --location westeurope --name myResourceGroup
+az group create --location "$location" --name $resourceGroup
 
 # Create an App Service plan in `FREE` tier.
-az appservice plan create --name myAppServicePlan --resource-group myResourceGroup --sku FREE
+az appservice plan create --name $appServicePlan --resource-group $resourceGroup --sku FREE
 
 # Create a web app.
-az webapp create --name $webappname --resource-group myResourceGroup --plan myAppServicePlan
+az webapp create --name $webapp --resource-group $resourceGroup --plan $appServicePlan
 
 # Get FTP publishing profile and query for publish URL and credentials
-creds=($(az webapp deployment list-publishing-profiles --name $webappname --resource-group myResourceGroup \
+creds=($(az webapp deployment list-publishing-profiles --name $webapp --resource-group $resourceGroup \
 --query "[?contains(publishMethod, 'FTP')].[publishUrl,userName,userPWD]" --output tsv))
 
 # Use cURL to perform FTP upload. You can use any FTP tool to do this instead. 
 curl -T index.html -u ${creds[1]}:${creds[2]} ${creds[0]}/
 
 # Copy the result of the following command into a browser to see the static HTML site.
-echo http://$webappname.azurewebsites.net
+site="http://$webapp.azurewebsites.net"
+echo $site
+curl "$site"
+# </FullScript>
+
+# echo "Deleting all resources"
+# az group delete --name $resourceGroup -y
