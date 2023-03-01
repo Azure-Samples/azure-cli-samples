b01d9fba5287f700daf852d944d3c3ef793ae8e2
Langlois,FX <82607691+flang-msft@users.noreply.github.com>
2023-03-01 12:44:00 -0800

    fxl---Fixed typos in script (#478)
    * Fixed typos in script
    
    * Updated test date

--- redis-cache/create-cache/create-manage-cache.sh
+++ redis-cache/create-cache/create-manage-cache.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-# Passed validation in Cloud Shell on 3/11/2022
+# Passed validation in Cloud Shell on 01/31/2023
 
 # <FullScript>
 # Create and manage a C0 Redis Cache
@@ -26,10 +26,10 @@ echo "Showing details of $cache"
 az redis show --name $cache --resource-group $resourceGroup 
 
 # Retrieve the hostname and ports for an Azure Redis Cache instance
-redis=($(az redis show --name $resourceGroup --resource-group $resourceGroup --query [hostName,enableNonSslPort,port,sslPort] --output tsv))
+redis=($(az redis show --name $cache --resource-group $resourceGroup --query [hostName,enableNonSslPort,port,sslPort] --output tsv))
 
 # Retrieve the keys for an Azure Redis Cache instance
-keys=($(az redis list-keys --name contosoCache --resource-group contosoGroup --query [primaryKey,secondaryKey] --output tsv))
+keys=($(az redis list-keys --name $cache --resource-group $resourceGroup --query [primaryKey,secondaryKey] --output tsv))
 
 # Display the retrieved hostname, keys, and ports
 echo "Hostname:" ${redis[0]}
