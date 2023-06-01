5b022864ea9fac8d4b1bb17073103254351c82d1
rogara <31490067+roygara@users.noreply.github.com>
2023-06-01 09:56:18 -0700

    Updating script to accommodate Premium SSD v2 and Ultra Disks. (#485)

--- virtual-machine/create-managed-disks-from-snapshot/create-managed-disks-from-snapshot.sh
+++ virtual-machine/create-managed-disks-from-snapshot/create-managed-disks-from-snapshot.sh
@@ -16,8 +16,12 @@ diskName=myDiskName
 #Provide the size of the disks in GB. It should be greater than the VHD file size.
 diskSize=128
 
-#Provide the storage type for Managed Disk. Premium_LRS or Standard_LRS.
+#Provide the storage type for Managed Disk. Acceptable values are Standard_LRS, Premium_LRS, PremiumV2_LRS, StandardSSD_LRS, UltraSSD_LRS, Premium_ZRS, and StandardSSD_ZRS.
 storageType=Premium_LRS
+
+#Required for Premium SSD v2 and Ultra Disks
+#Provide the Availability Zone you'd like the disk to be created in, default is 1
+zone=1
 
 #Set the context to the subscription Id where Managed Disk will be created
 az account set --subscription $subscriptionId
@@ -27,5 +31,6 @@ snapshotId=$(az snapshot show --name $snapshotName --resource-group $resourceGro
 
 #Create a new Managed Disks using the snapshot Id
 #Note that managed disk will be created in the same location as the snapshot
+#If you're creating a Premium SSD v2 or an Ultra Disk, add "--zone $zone" to the end of the command
 az disk create --resource-group $resourceGroupName --name $diskName --sku $storageType --size-gb $diskSize --source $snapshotId
 # </FullScript>
