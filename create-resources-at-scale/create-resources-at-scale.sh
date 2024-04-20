823700fe7704468d71fe79ae05a5a5876940ff3f
Jerry Li <jli@jerry-li.com>
2024-04-19 17:22:13 -0700

    removing blank lines

--- create-resources-at-scale/create-resources-at-scale.sh
+++ create-resources-at-scale/create-resources-at-scale.sh
@@ -17,31 +17,20 @@ location=""
 resourceGroup="msdocs-ubuntu-vm-group22-$Identifier"
 
 # task 1 create resources
-
 for i in $(seq 0 1);
 do
     # Read in commands from csv file
-
     echo read in commands from csv file
-
     c1= sed -n "$Identifier"p file.csv
-
     Identifier=$((Identifier+1))
     echo $Identifier. Creating resources for $user
-
     resourceGroup="ubuntu-vm-group22-$Identifier"
     subnet="msdocs-vnet-$Identifier"
-
     az group create --l $location -n $resourceGroup
-
     echo $Identifier
-
     echo test echo
-
     if "$createvm" =true; then 
-
         echo create vm
-
         az vm create \
             --resource-group $resourceGroup \
             --name $vmName \
@@ -50,24 +39,16 @@ do
             --admin-username $user\
             --admin-password qwertY123456  
     fi
-
     if "$createvnet" =true; then 
-
     echo create vnet
-
-    az network vnet create \
-    
+    az network vnet create \    
         --name $vnetname \
         --resource-group $resourceGroup \
         --address-prefix 10.0.0.0/16 \
         --subnet-name $subnet-$Identifier \
         --subnet-prefixes 10.0.0.0/24
-
-    fi
-    
+    fi    
     az group delete -n $resourceGroup -y
-
     echo deleted $resourceGroup
-
     done 2>&1 >> output.txt
 echo done
