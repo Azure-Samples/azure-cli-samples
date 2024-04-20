a10802be59a857b7f811350aa0c738c6601976e3
Jerry Li <jli@jerry-li.com>
2024-04-19 17:07:10 -0700

    add variables to csv file

--- create-resources-at-scale/create-resources-at-scale.sh
+++ create-resources-at-scale/create-resources-at-scale.sh
@@ -2,65 +2,46 @@
 
 # Passed validation in Azure Cloud Shell Bash environment on 4/25/2022
 
-# <FullScript>
-
-# One sentence script description here
-
-#
-
-# Scripts steps expalined here
-
-# ...
-
-# Finally, it ...
-
-#
-
-# set -e # exit if error
-
 # Variable block
-
-
+# These variables have placeholder values that are replaced with values from the csv input file.
 Identifier="0"
-
-user="john"
-
+user=""
 createvm="true"
-
-vmName="linuxVM-$user"
-
+createvnet="true"
+createrg="true"
+subnet=""
+vmName="msdocs-linuxVM-$user"
+vnetname=""
 vmImage="Ubuntu2204"
-
-location="westus"
-
-resourceGroup="ubuntu-vm-group22-$Identifier"
+location=""
+resourceGroup="msdocs-ubuntu-vm-group22-$Identifier"
 
 # task 1 create resources
 
 for i in $(seq 0 1);
 do
-    Identifier=$((Identifier+1))
-    echo "creating task $Identifier that creates a [resource group]"
-
-    echo random $Identifier
-
-    resourceGroup="ubuntu-vm-group22-$Identifier"
-
-    az group create --l $location -n $resourceGroup
-
-    echo $Identifier
-
-    # read in commands from csv file
+    # Read in commands from csv file
 
     echo read in commands from csv file
 
     c1= sed -n "$Identifier"p file.csv
 
+    Identifier=$((Identifier+1))
+    echo $Identifier. Creating resources for $user
+
+    resourceGroup="ubuntu-vm-group22-$Identifier"
+    subnet="msdocs-vnet-$Identifier"
+
+    az group create --l $location -n $resourceGroup
+
+    echo $Identifier
+
     echo test echo
 
     if "$createvm" =true; then 
 
-        echo loop
+        echo create vm
+
         az vm create \
             --resource-group $resourceGroup \
             --name $vmName \
@@ -69,6 +50,21 @@ do
             --admin-username $user\
             --admin-password qwertY123456  
     fi
+
+    if "$createvnet" =true; then 
+
+    echo create vnet
+
+    az network vnet create \
+    
+        --name $vnetname \
+        --resource-group $resourceGroup \
+        --address-prefix 10.0.0.0/16 \
+        --subnet-name $subnet-$Identifier \
+        --subnet-prefixes 10.0.0.0/24
+
+    fi
+    
     az group delete -n $resourceGroup -y
 
     echo deleted $resourceGroup
