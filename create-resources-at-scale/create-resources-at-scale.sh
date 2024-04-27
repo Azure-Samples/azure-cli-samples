1f39245b04745045b7e501aed2c6f19ac186cac1
Jerry Li <jli@jerry-li.com>
2024-04-26 17:22:23 -0700

    removed top row from the csv file

--- create-resources-at-scale/create-resources-at-scale.sh
+++ create-resources-at-scale/create-resources-at-scale.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+ 
 # Passed validation in Azure Cloud Shell Bash environment on 4/25/2022
-
+ 
 # Variable block
 # These variables have placeholder values that are replaced with values from the csv input file.
-subscriptionId="3618afcd-ea52-4ceb-bb46-53bb962d4e0b"
+subscriptionID=3618afcd-ea52-4ceb-bb46-53bb962d4e0b
 lineCounter="0"
-randomIdentifier="$RANDOM*$RANDOM"
+randomIdentifier=$RANDOM
 user=""
 adminPassword="msdocs-script-PS-$randomIdentifier"
 createvm="true"
@@ -14,29 +14,27 @@ createvnet="true"
 createrg="true"
 subnet=""
 vmName="msdocs-linuxVM-$user"
-vnetname=""
+vnetname="msdocs-vnet-$user-$randomIdentifier"
 vmImage="Ubuntu2204"
 location=""
-resourceGroup="msdocs-ubuntu-vm-group22-$Identifier"
-setupFileLocation="C:\myPath\myFileName"
+resourceGroup="msdocs-ubuntu-vm-group22-$randomIdentifier"
+setupFileLocation="resource-metadata.csv"
 publicIpSku=""
 addressPrefix="10.0.0.0/16"
 subnetPrefixes="10.0.0.0/24"
-
-# select azure subscription 
-
-az account set --subscription "$subscriptionId"
-
-# task 1 create resources
-for i in $(seq 0 1);
+ 
+# select azure subscription
+az account set --subscription $subscriptionID
+ 
+OLDIFS=$IFS
+IFS=','
+[ ! -f $setupFileLocation ] && { echo "$setupFileLocation file not found"; exit 99; }
+while read resourceNo createRG createVnet createVM location user subnet vmImage publicIpSku
 do
-    # Read in commands from csv file
-    echo Read in commands from csv file
-    c1= sed -n "$lineCounter"p $setupFileLocation
-    lineCounter=$((lineCounter+1))
-    echo $lineCounter. Creating resources for $user
-    resourceGroup="msdocs-script-rg-$randomIdentifier"
-    subnet="msdocs-vnet-$randomIdentifier"
+	echo $resourceNo
+	echo createRG
+    echo "location is $location"
+    printf $location
     az group create --l $location -n $resourceGroup
     if "$createvm" =true; then 
         echo create vm
@@ -48,6 +46,7 @@ do
             --admin-username $user\
             --admin-password $adminPassword  
     fi
+	echo $createVnet
     if "$createvnet" =true; then 
     echo create vnet
     az network vnet create \    
@@ -56,8 +55,22 @@ do
         --address-prefix $addressPrefix \
         --subnet-name $subnet-$randomIdentifier \
         --subnet-prefixes $subnetPrefixes
-    fi    
-    az group delete -n $resourceGroup -y
-    echo deleted $resourceGroup
-    done 2>&1 >> output.txt
-echo done
+    fi  
+	echo Create $createVM
+    if "$createvm" =true; then 
+        echo create vm
+        az vm create \
+            --resource-group $resourceGroup \
+            --name $vmName \
+            --image $vmImage \
+            --public-ip-sku $publicIpSku \
+            --admin-username $user\
+            --admin-password $adminPassword  
+    fi
+	echo $location
+	echo $user
+	echo $subnet
+	echo $vmImage
+	echo $publicIpSku
+done < $setupFileLocation
+IFS=$OLDIFS
