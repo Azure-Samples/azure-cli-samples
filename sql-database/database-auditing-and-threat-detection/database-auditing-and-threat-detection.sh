dae7b24053e32f5ca982901df57b6b29bf5b2c3f
Delora Bradish <dbradish@microsoft.com>
2025-01-02 12:53:47 -0500

    database-auditing-and-threat-detection.sh | removed deprecated command (#535)
    * database-auditing-and-threat-detection.sh | removed deprecated reference command
    
    The [az sql db threat-policy update]() is implicitly deprecated because command group 'sql db threat-policy' is deprecated and will be removed in a future release. Use '[sql db advanced-threat-protection-setting](https://learn.microsoft.com/en-us/cli/azure/sql/db/advanced-threat-protection-setting?view=azure-cli-latest#az-sql-db-advanced-threat-protection-setting-update)' instead.
    
    * Add comment for setting threat detection policy

--- sql-database/database-auditing-and-threat-detection/database-auditing-and-threat-detection.sh
+++ sql-database/database-auditing-and-threat-detection/database-auditing-and-threat-detection.sh
@@ -33,8 +33,7 @@ az storage account create --name $storage --resource-group $resourceGroup --loca
 echo "Setting access policy on $storage..."
 az sql db audit-policy update --name $database --resource-group $resourceGroup --server $server --state Enabled --bsts Enabled --storage-account $storage
 
-echo "Setting threat detection policy on $storage..."
-az sql db threat-policy update --email-account-admins Disabled --email-addresses $notification --name $database --resource-group $resourceGroup --server $server --state Enabled --storage-account $storage
+# use `az sql db advanced-threat-protection-setting update` to set the threat detection policy on $storage
 
 # </FullScript>
 
