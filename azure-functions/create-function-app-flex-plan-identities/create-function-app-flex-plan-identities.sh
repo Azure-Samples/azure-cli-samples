a6c94ed10b5347829a1bf671593de6f8056d30b6
Glenn Gailey <ggailey777@hotmail.com>
2025-05-15 12:55:13 -0700

    Add new Flex with UAMI script + snippet source file

---
+++ azure-functions/create-function-app-flex-plan-identities/create-function-app-flex-plan-identities.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Passed validation in Cloud Shell on 3/24/2022
+
+# <FullScript>
+# Function app, storage account, and user identity names must be unique.
+
+# Variable block
+let "randomIdentifier=$RANDOM*$RANDOM"
+location="northeurope"
+resourceGroup="msdocs-azure-functions-rg-$randomIdentifier"
+tag="create-function-app-flex-plan-identities"
+storage="msdocsaccount$randomIdentifier"
+userIdentity="msdocs-managed-identity-$randomIdentifier"
+functionApp="msdocs-serverless-function-$randomIdentifier"
+skuStorage="Standard_LRS"
+functionsVersion="4"
+languageWorker="dotnet-isolated"
+languageVersion="8.0"
+
+# Install the Application Insights extension
+az extension add --name application-insights
+
+# Create a resource group
+echo "Creating $resourceGroup in "$location"..."
+az group create --name $resourceGroup --location "$location" --tags $tag
+
+# Create an Azure storage account in the resource group with key access disabled.
+echo "Creating $storage"
+az storage account create --name $storage --location "$location" --resource-group $resourceGroup \
+    --sku $skuStorage --allow-blob-public-access false --allow-shared-key-access false
+
+# Create a user-assigned managed identity
+echo "Creating $userIdentity"
+output=$(az identity create --name $userIdentity --resource-group $resourceGroup --location $location \
+    --query "{userId:id, principalId: principalId, clientId: clientId}" -o json)
+
+# Use jq to parse the output and assign the properties to variables
+userId=$(echo $output | jq -r '.userId')
+principalId=$(echo $output | jq -r '.principalId')
+clientId=$(echo $output | jq -r '.clientId')
+
+# Get the storage ID and create a role assignment (Storage Blob Data Owner) for the user
+storageId=$(az storage account show --resource-group $resourceGroup --name $storage --query 'id' -o tsv)
+az role assignment create --assignee-object-id $principalId --assignee-principal-type ServicePrincipal \
+    --role "Storage Blob Data Owner" --scope $storageId
+
+# Create the function app in a Flex Consumption plan that uses the user-assigned managed identity
+# to access the deployment share.
+az functionapp create --resource-group $resourceGroup --name $functionApp --flexconsumption-location $location  \
+    --runtime $languageWorker --runtime-version $languageVersion --storage-account $storage \
+    --deployment-storage-auth-type UserAssignedIdentity --deployment-storage-auth-value $userIdentity 
+
+# Create a role assigment (Monitoring Metrics Publisher) in Application Insights for the user identity
+appInsights=$(az monitor app-insights component show --resource-group $resourceGroup \
+    --app $functionApp --query "id" --output tsv)
+az role assignment create --role "Monitoring Metrics Publisher" --assignee $principalId --scope $appInsights
+
+# Update app settings to use managed identities for all connections
+az functionapp config appsettings set --name $functionApp --resource-group $resourceGroup \
+    --settings AzureWebJobsStorage__accountName=$storage AzureWebJobsStorage__credential=managedidentity \
+    AzureWebJobsStorage__clientId=$clientId APPLICATIONINSIGHTS_AUTHENTICATION_STRING="ClientId=$clientId;Authorization=AAD"
+az functionapp config appsettings delete --name $functionApp --resource-group $resourceGroup --setting-names AzureWebJobsStorage
+# </FullScript>
+
+# echo "Deleting all resources"
+# az group delete --name $resourceGroup -y
