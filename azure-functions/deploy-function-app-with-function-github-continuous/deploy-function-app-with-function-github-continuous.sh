33a18d8ba69ea103d82aca39c80200d26768a779
Glenn Gailey <ggailey777@hotmail.com>
2026-02-28 11:04:13 -0800

    Change GH deployment script back to Consumption plan and remove unused identity management code

--- azure-functions/deploy-function-app-with-function-github-continuous/deploy-function-app-with-function-github-continuous.sh
+++ azure-functions/deploy-function-app-with-function-github-continuous/deploy-function-app-with-function-github-continuous.sh
@@ -1,69 +1,35 @@
 #!/bin/bash
 # TODO: Validate in Cloud Shell before merging
 
-# Function app, storage account, and user identity names must be unique.
+# Function app and storage account names must be unique.
 let "randomIdentifier=$RANDOM*$RANDOM"
 location="eastus"
 resourceGroup="msdocs-azure-functions-rg-$randomIdentifier"
 tag="deploy-function-app-with-function-github"
 storage="msdocsaccount$randomIdentifier"
-userIdentity="msdocs-managed-identity-$randomIdentifier"
 functionApp="msdocs-serverless-function-$randomIdentifier"
 skuStorage="Standard_LRS"
 functionsVersion="4"
-languageWorker="node"
-languageVersion="20"
+runtime="node"
+runtimeVersion="20"
 # Public GitHub repository containing an Azure Functions code project.
 gitrepo=https://github.com/Azure-Samples/functions-quickstart-javascript
-
-# Install the Application Insights extension
-az extension add --name application-insights
 
 # Create a resource group
 echo "Creating $resourceGroup in "$location"..."
 az group create --name $resourceGroup --location "$location" --tags $tag
 
-# Create an Azure storage account in the resource group with key access disabled.
+# Create an Azure storage account in the resource group.
 echo "Creating $storage"
-az storage account create --name $storage --location "$location" --resource-group $resourceGroup \
-    --sku $skuStorage --allow-blob-public-access false --allow-shared-key-access false
+az storage account create --name $storage --location "$location" --resource-group $resourceGroup --sku $skuStorage
 
-# Create a user-assigned managed identity
-echo "Creating $userIdentity"
-output=$(az identity create --name $userIdentity --resource-group $resourceGroup --location $location \
-    --query "{userId:id, principalId: principalId, clientId: clientId}" -o json)
-
-# Use jq to parse the output and assign the properties to variables
-userId=$(echo $output | jq -r '.userId')
-principalId=$(echo $output | jq -r '.principalId')
-clientId=$(echo $output | jq -r '.clientId')
-
-# Get the storage ID and create a role assignment (Storage Blob Data Owner) for the identity
-storageId=$(az storage account show --resource-group $resourceGroup --name $storage --query 'id' -o tsv)
-az role assignment create --assignee-object-id $principalId --assignee-principal-type ServicePrincipal \
-    --role "Storage Blob Data Owner" --scope $storageId
-
-# Create the function app in a Flex Consumption plan with source files deployed from GitHub
+# Create a serverless function app in the resource group.
 echo "Creating $functionApp"
-az functionapp create --resource-group $resourceGroup --name $functionApp --flexconsumption-location $location \
-    --runtime $languageWorker --runtime-version $languageVersion --storage-account $storage \
-    --deployment-storage-auth-type UserAssignedIdentity --deployment-storage-auth-value $userIdentity \
+az functionapp create --name $functionApp --storage-account $storage \
+    --consumption-plan-location "$location" --resource-group $resourceGroup \
+    --runtime $runtime --runtime-version $runtimeVersion \
+    --functions-version $functionsVersion \
     --deployment-source-url $gitrepo --deployment-source-branch main
-
-# Create a role assignment (Monitoring Metrics Publisher) in Application Insights for the user identity
-appInsights=$(az monitor app-insights component show --resource-group $resourceGroup \
-    --app $functionApp --query "id" --output tsv)
-az role assignment create --role "Monitoring Metrics Publisher" --assignee $principalId --scope $appInsights
-
-# Update app settings to use managed identities for host storage connections
-clientId=$(az identity show --name $userIdentity --resource-group $resourceGroup \
-    --query 'clientId' -o tsv)
-az functionapp config appsettings set --name $functionApp --resource-group $resourceGroup \
-    --settings AzureWebJobsStorage__accountName=$storage AzureWebJobsStorage__credential=managedidentity \
-    AzureWebJobsStorage__clientId=$clientId \
-    APPLICATIONINSIGHTS_AUTHENTICATION_STRING="ClientId=$clientId;Authorization=AAD"
-az functionapp config appsettings delete --name $functionApp \
-    --resource-group $resourceGroup --setting-names AzureWebJobsStorage
 
 # echo "Deleting all resources"
 # az group delete --name $resourceGroup -y
